ee7027a737974eb63c22ca34bc77aba7b5ba3d91
TrevisanGMW <trevisangmw@gmail.com>
2022-07-04 20:56:34 -0700

    Small PEP8 changes

--- python-scripts/gt_rigger_game_exporter.py
+++ python-scripts/gt_rigger_game_exporter.py
@@ -5,33 +5,34 @@ v1.0.0 - Initial Release
 
 v1.0.1 - Removed namespaces before exporting
 """
-import maya.api.OpenMaya as om
+import maya.api.OpenMaya as OpenMaya
 import maya.cmds as cmds
 import maya.mel as mel
 import logging
 import sys
 
 from gt_rigger_utilities import find_joint, find_transform, get_metadata, select_items, get_children
-from functools import partial
-from collections import namedtuple
 from gt_tools.gt_utilities import make_flat_list
+from collections import namedtuple
+from functools import partial
 
 SCRIPT_VERSION = '1.0.1'
 SCRIPT_NAME = 'GT Rigger - Game Exporter'
 logging.basicConfig()
-logger = logging.getLogger("exporter")
+logger = logging.getLogger("gt_rigger_game_exporter")
+logger.setLevel(20)
 
 
-def _get_object_namespaces(objectName):
+def _get_object_namespaces(object_name):
     """
     Returns only the namespace of the object
     Args:
-        objectName (string): Name of the object to extract the namespace from
+        object_name (string): Name of the object to extract the namespace from
     Returns:
         namespaces (string): Extracted namespaces combined into a string (without the name of the object)
                              e.g. Input = "One:Two:pSphere" Output = "One:Two:"
     """
-    namespaces_list = objectName.split(':')
+    namespaces_list = object_name.split(':')
     object_namespace = ''
     for namespace in namespaces_list:
         if namespace != namespaces_list[-1]:
@@ -58,8 +59,8 @@ class StripNamespace(object):
         """
         Convenience method to extract the name from uuid
 
-        :type uuid: basestring
-        :rtype: unicode|None
+        type uuid: basestring
+        rtype: unicode|None
         """
         names = cmds.ls(uuid)
         return names[0] if names else None
@@ -79,8 +80,8 @@ class StripNamespace(object):
 
                 # get an api handle to the node
                 try:
-                    api_obj = om.MGlobal.getSelectionListByName(absolute_name).getDependNode(0)
-                    api_node = om.MFnDependencyNode(api_obj)
+                    api_obj = OpenMaya.MGlobal.getSelectionListByName(absolute_name).getDependNode(0)
+                    api_node = OpenMaya.MFnDependencyNode(api_obj)
 
                     # Remember the original name to return upon exit
                     uuid = api_node.uuid().asString()
@@ -98,57 +99,58 @@ class StripNamespace(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         for uuid, original_name in self.original_names.items():
             current_name = self.as_name(uuid)
-            api_obj = om.MGlobal.getSelectionListByName(current_name).getDependNode(0)
-            api_node = om.MFnDependencyNode(api_obj)
+            api_obj = OpenMaya.MGlobal.getSelectionListByName(current_name).getDependNode(0)
+            api_node = OpenMaya.MFnDependencyNode(api_obj)
             api_node.setName(original_name)
 
 
-def _export_fbx(file_path, export_baked_animation=True):
-    '''
+def _export_fbx(file_path, baked_animation_export=True):
+    """
     Exports auto biped rig data as FBX to be imported into real-time engines.
     This function was specifically made for rigs created with GT biped rigger it assumes that
     all geometry will be found inside "geometry_grp" and the bound skeleton is "root_jnt"
 
     Args:
         file_path (string) : Path to export location. For example "C:\\character.fbx"
-        export_baked_animation (optional, bool) : If active, animation will be baked and geometry will be ignored.
+        baked_animation_export (optional, bool) : If active, animation will be baked and geometry will be ignored.
                                                   Only skeleton and animation are exported with this option.
                                                   If deactivated, then skeleton and geometry will be exported
                                                   (no animation baking)
 
     Returns:
         response (bool) : True if operation was successful and False if it failed.
-    '''
-    prerolldata = export_preroll()
-    configureFBX()
-    if export_baked_animation:
-        exportBakedAnimation()
-        select_items(prerolldata.root)
+    """
+    pre_roll_data = export_pre_roll()
+    configure_fbx()
+    if baked_animation_export:
+        baked_animation_export()
+        select_items(pre_roll_data.root)
     else:
-        select_items(prerolldata.geo, prerolldata.root)
+        select_items(pre_roll_data.geo, pre_roll_data.root)
 
     # Handle Namespaces in References
-    namespace = _get_object_namespaces(prerolldata.root)
+    namespace = _get_object_namespaces(pre_roll_data.root)
     if namespace:
         with StripNamespace(namespace) as stripped_nodes:
             cmds.FBXExport('-file', file_path, '-s')
+            logger.debug(stripped_nodes)
     else:
         cmds.FBXExport('-file', file_path, '-s')
 
-    _set_stored_attributes(prerolldata.attrs)
+    _set_stored_attributes(pre_roll_data.attrs)
     return True
 
 
-def export_preroll():
+def export_pre_roll():
     """
-    does a series of preroll steps to find the parts and set some fbx attrs
+    does a series of pre-roll steps to find the parts and set some fbx attrs
     Returns:
-        PrerollData namedtuple:
+        Pre-rollData namedtuple:
             root: <str> root item
             geo: <str> of geo group
             attrs: <dict> of the attrs that were made visible, so we can set them back after
     """
-    prerollData = namedtuple("PrerollData", ['root', 'geo', 'attrs'])
+    pre_roll_data = namedtuple("PrerollData", ['root', 'geo', 'attrs'])
 
     _root = find_root()
     if _root is None:
@@ -162,33 +164,35 @@ def export_preroll():
     if _skeleton is None:
         return False
 
-    if not fbxPluginLoaded():
+    if not fbx_plugin_loaded():
         return False
 
     attr_state_dict = _make_visible(_geo, _skeleton, _root)
 
-    return prerollData(root=_root, geo=getChildren(_geo), attrs=attr_state_dict)
+    return pre_roll_data(root=_root, geo=get_children(_geo), attrs=attr_state_dict)
 
 
-def fbxPluginLoaded():
+def fbx_plugin_loaded():
     """simple checker for the fbx loaded status"""
     try:
         cmds.FBXExport
-    except:
+    except Exception as e:
+        logger.debug(str(e))
         try:
             cmds.loadPlugin('fbxmaya')
-        except:
+        except Exception as e:
+            logger.debug(str(e))
             sys.stderr.write('ERROR: FBX Export Plug-in was not detected.\n')
             return False
     return True
 
 
-def exportBakedAnimation():
-    fbxPluginLoaded()
-    setFBXProperty('FBXExportBakeComplexAnimation', 'true')
+def export_baked_animation():
+    fbx_plugin_loaded()
+    set_fbx_property('FBXExportBakeComplexAnimation', 'true')
 
 
-def setFBXProperty(name, value):
+def set_fbx_property(name, value):
     _propString = "{name} -v {value};".format(name=name, value=value)
     try:
         mel.eval(_propString)
@@ -198,18 +202,18 @@ def setFBXProperty(name, value):
         logger.warning(str(e))
 
 
-def setFBXGeometryProperty(name, value):
+def set_fbx_geometry_property(name, value):
     _fullname = f"FBXProperty Export|IncludeGrp|Geometry|{name}"
-    setFBXProperty(name=_fullname, value=value)
+    set_fbx_property(name=_fullname, value=value)
 
 
-def setFBXExportProperty(name, value):
+def set_fbx_export_property(name, value):
     _fullname = f"FBXExport{name}"
-    setFBXProperty(name=_fullname, value=value)
+    set_fbx_property(name=_fullname, value=value)
 
 
-def configureFBX():
-    fbxPluginLoaded()
+def configure_fbx():
+    fbx_plugin_loaded()
     # Configure FBX export settings
     # https://docs.unity3d.com/2017.4/Documentation/Manual/HOWTO-ArtAssetBestPracticeGuide.html
     # https://docs.unity3d.com/560/Documentation/Manual/HOWTO-exportFBX.html
@@ -228,9 +232,9 @@ def configureFBX():
         "ContainerObjects": "true",
     }
     for _name, _value in _geo_properties.items():
-        setFBXGeometryProperty(_name, _value)
+        set_fbx_geometry_property(_name, _value)
 
-    setFBXGeometryProperty("GeometryNurbsSurfaceAs", '\"Interactive Display Mesh\"')
+    set_fbx_geometry_property("GeometryNurbsSurfaceAs", '\"Interactive Display Mesh\"')
 
     _export_properties = {
         "ReferencedAssetsContent": "false",
@@ -248,7 +252,7 @@ def configureFBX():
 
     # Ignore unnecessary elements
     for _name, _value in _export_properties.items():
-        setFBXExportProperty(_name, _value)
+        set_fbx_export_property(_name, _value)
 
 
 def find_root():
@@ -279,8 +283,7 @@ def _make_visible(*args):
             cmds.setAttr(obj + '.overrideEnabled', 0)
             cmds.setAttr(obj + '.v', 1)
         except Exception as e:
-
-            pass
+            logger.debug(str(e))
     return attr_state_dict
 
 
@@ -293,15 +296,15 @@ def _set_stored_attributes(attr_state_dict):
     for key, value in attr_state_dict.items():
         try:
             cmds.setAttr(key, value)
-        except:
-            pass
+        except Exception as e:
+            logger.debug(str(e))
 
 
 def _get_skeleton_root_from_metadata():
     """
     looks for the metadata and if it finds it, return the root value
     Returns:
-        <str> or None if cant find
+        <str> or None if it can't find the metadata
     """
     _data = get_metadata(object_name=find_main_ctrl())
     if _data:
@@ -312,17 +315,17 @@ def _validate_scene():
     """Returns true if all necessary elements are present in the scene"""
     _root = find_root()
     if _root is None:
-        cmds.warning('Script couldn\'t find "root_jnt" joint. Make sure you have a valid scene opened.')
+        cmds.warning("Script couldn't find \"root_jnt\" joint. Make sure you have a valid scene opened.")
         return False
 
     _geo = find_geo_grp()
     if _geo is None:
-        cmds.warning('Script couldn\'t find geometry group. Make sure you have a valid scene opened.')
+        cmds.warning("Script couldn't find geometry group. Make sure you have a valid scene opened.")
         return False
 
     _skeleton = find_skeleton_grp()
     if _skeleton is None:
-        cmds.warning('Script couldn\'t find skeleton group. Make sure you have a valid scene opened.')
+        cmds.warning("Script couldn't find skeleton group. Make sure you have a valid scene opened.")
         return False
     return True
 
@@ -338,24 +341,28 @@ def _export_fbx_file_dialog(caption_description='Model'):
         file_name = cmds.fileDialog2(fileFilter='FBX File (*.fbx)',
                                      dialogStyle=2,
                                      okCaption='Export',
-                                     caption='Exporting ' + caption_description + ' (FBX) file for a WideAwake Rig') or []
+                                     caption='Exporting ' + caption_description +
+                                             ' (FBX) file for a WideAwake Rig') or []
         if len(file_name) > 0:
             return file_name[0]
 
 
 def _export_fbx_model(*args):
+    logger.debug(str(*args))
     fbx_path = _export_fbx_file_dialog()
     if fbx_path:
-        _export_fbx(fbx_path, export_baked_animation=False)
+        _export_fbx(fbx_path, baked_animation_export=False)
 
 
 def _export_fbx_animation(*args):
+    logger.debug(str(*args))
     fbx_path = _export_fbx_file_dialog('Animation')
     if fbx_path:
-        _export_fbx(fbx_path, export_baked_animation=True)
+        _export_fbx(fbx_path, baked_animation_export=True)
 
 
 def _build_gui_help_fbx_exporter(*args):
+    logger.debug(str(*args))
     cmds.warning('No help yet. It will be here soon')
 
 
@@ -410,13 +417,13 @@ if __name__ == '__main__':
 
     # # Export Model
     # temp_file = 'C:\\Users\\guilherme.trevisan\\Desktop\\model.fbx'
-    # reponse = _export_fbx(temp_file, export_baked_animation=False)
-    # print(str(reponse))
+    # response = _export_fbx(temp_file, export_baked_animation=False)
+    # print(str(response))
 
     # # Export Animation
     # temp_file = 'C:\\Users\\guilherme.trevisan\\Desktop\\animation.fbx'
-    # reponse = _export_fbx(temp_file, export_baked_animation=True)
-    # print(str(reponse))
+    # response = _export_fbx(temp_file, export_baked_animation=True)
+    # print(str(response))
 
     # output = _export_fbx_file_dialog()
     # print(output)
