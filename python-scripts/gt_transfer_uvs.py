3d589a54043202095a80c95d37bfaf9b9eb0c463
TrevisanGMW <trevisangmw@gmail.com>
2022-07-11 12:15:36 -0700

    Small logging level update

--- python-scripts/gt_transfer_uvs.py
+++ python-scripts/gt_transfer_uvs.py
@@ -42,7 +42,7 @@ import sys
 # Logging Setup
 logging.basicConfig()
 logger = logging.getLogger("gt_transfer_uvs")
-logger.setLevel(20)  # DEBUG 10, INFO 20, WARNING 30, ERROR 40, CRITICAL 50
+logger.setLevel(logging.INFO)
 
 # Script Name
 script_name = 'GT - Transfer UVs'
