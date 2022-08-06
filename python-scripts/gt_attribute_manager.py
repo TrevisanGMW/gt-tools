6d1e6080bba99f39cc2b27b7adf48fc1e1ed4763
TrevisanGMW <trevisangmw@gmail.com>
2022-08-05 19:24:10 -0700

    Initial upload

---
+++ python-scripts/gt_attribute_manager.py
@@ -0,0 +1,16 @@
+'''
+
+Script would work exactly like the "Add attribute" function, but it would retain the parameters and allow for multiple
+variables (separated by commas)
+
+Consider adding:
+
+Textfield with list of affected attributes.
+Hide/unhide attributes for selected elements.
+Lock/unlock attributes for selected elements.
+Auto create a list of attributes for selected elements.
+Auto delete a list of attributes for selected elements.
+Maybe attempt to change the order of the attributes within Maya.
+
+
+'''
