62a6af43576cd57b3daaa169e86875be9e3c5c30
TrevisanGMW <trevisangmw@gmail.com>
2022-08-06 11:33:36 -0700

    Started function to add attributes

--- python-scripts/gt_attribute_manager.py
+++ python-scripts/gt_attribute_manager.py
@@ -10,3 +10,28 @@ Maybe attempt to change the order of the attributes within Maya.
 
 
 """
+from collections import namedtuple
+import maya.cmds as cmds
+
+
+def add_attributes(target_list, attributes):
+    for target_obj in target_list:
+        for attr in attributes:
+            print(attr)
+            # cmds.addAttr(target_obj, ln=attr, at='double', k=True)
+
+
+if __name__ == '__main__':
+
+    # Pose Object Setup
+    Attribute = namedtuple('Attribute', ['name', 'type'])
+    attributes = []
+
+    attributes += [
+        Attribute(name='attrOne',
+                  type='double'),
+        Attribute(name='attrTwo',
+                  type='double')
+                  ]
+
+    add_attributes(['abc'], attributes)
