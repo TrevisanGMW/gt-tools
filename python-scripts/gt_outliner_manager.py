c50cd50a9aae38796ec2683a68ecfa3f85264232
TrevisanGMW <trevisangmw@gmail.com>
2022-08-20 12:52:07 -0700

    Added reorder utility functions

--- python-scripts/gt_outliner_manager.py
+++ python-scripts/gt_outliner_manager.py
@@ -15,6 +15,86 @@ logger = logging.getLogger("gt_outliner_manager")
 logger.setLevel(logging.INFO)
 
 
+def reorder_up(obj_list):
+    """
+    Reorder objects in the outliner relative to their siblings (move them up)
+    Args:
+        obj_list: List of objects to be reordered (not existing objects are ignored)
+
+    Returns:
+        operation_result: True = at least one object was updated, False = unable to find valid objects
+    """
+    valid_obj_list = []
+    for obj in obj_list:
+        if cmds.objExists(obj):
+            valid_obj_list.append(obj)
+    cmds.reorder(valid_obj_list, relative=-1)
+    if valid_obj_list:
+        return True
+    else:
+        return False
+
+
+def reorder_down(obj_list):
+    """
+    Reorder objects in the outliner relative to their siblings (move them down)
+    Args:
+        obj_list: List of objects to be reordered (not existing objects are ignored)
+
+    Returns:
+        operation_result: True = at least one object was updated, False = unable to find valid objects
+    """
+    valid_obj_list = []
+    for obj in obj_list:
+        if cmds.objExists(obj):
+            valid_obj_list.append(obj)
+    cmds.reorder(valid_obj_list, relative=1)
+    if valid_obj_list:
+        return True
+    else:
+        return False
+
+
+def reorder_front(obj_list):
+    """
+    Reorder objects in the outliner relative to their siblings (move them to the top)
+    Args:
+        obj_list: List of objects to be reordered (not existing objects are ignored)
+
+    Returns:
+        operation_result: True = at least one object was updated, False = unable to find valid objects
+    """
+    valid_obj_list = []
+    for obj in obj_list:
+        if cmds.objExists(obj):
+            valid_obj_list.append(obj)
+    cmds.reorder(valid_obj_list, front=True)
+    if valid_obj_list:
+        return True
+    else:
+        return False
+
+
+def reorder_back(obj_list):
+    """
+    Reorder objects in the outliner relative to their siblings (move them to the bottom)
+    Args:
+        obj_list: List of objects to be reordered (not existing objects are ignored)
+
+    Returns:
+        operation_result: True = at least one object was updated, False = unable to find valid objects
+    """
+    valid_obj_list = []
+    for obj in obj_list:
+        if cmds.objExists(obj):
+            valid_obj_list.append(obj)
+    cmds.reorder(valid_obj_list, back=True)
+    if valid_obj_list:
+        return True
+    else:
+        return False
+
+
 def outliner_sort(target_list):
     logger.debug('target_list: ' + str(target_list))
     issues = ''
@@ -37,4 +117,7 @@ def outliner_sort(target_list):
 
 if __name__ == '__main__':
     selection = cmds.ls(selection=True)
+    # cmds.reorder(selection[0], front=True)
+    # reorder_up(selection)
+    # reorder_back(selection)
     outliner_sort(selection)
