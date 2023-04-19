24bac66f9c49dd42410f510f7f25be350d0b1d6c
Guilherme Trevisan <trevisangmw@gmail.com>
2023-04-19 16:20:12 -0700

    Created first color utilities

--- tests/test_utils/test_color_utils.py
+++ tests/test_utils/test_color_utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import unittest
+import maya.cmds as cmds
 from collections import namedtuple
 
 # Logging Setup
@@ -10,18 +11,20 @@ logger = logging.getLogger("test_session_utils")
 logger.setLevel(logging.DEBUG)
 
 # Import Test Session Utilities
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-#from tools.validation import validation_cleanup
+tools_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+if tools_root_dir not in sys.path:
+    sys.path.append(tools_root_dir)
+from utils import color_utils
 
 
-# try:
-#     import maya.cmds as cmds
-#     import maya.standalone
-#     import maya.mel as mel
-#     import maya.OpenMaya as OpenMaya
-# except Exception as e:
-#     logger.debug(str(e))
-#     logger.warning("Unable load maya cmds, maya standalone, mel or OpenMaya")
+try:
+    import maya.cmds as cmds
+    import maya.standalone
+    import maya.mel as mel
+    import maya.OpenMaya as OpenMaya
+except Exception as e:
+    logger.debug(str(e))
+    logger.warning("Unable load maya cmds, maya standalone, mel or OpenMaya")
 
 
 class TestColorUtils(unittest.TestCase):
@@ -35,9 +38,18 @@ class TestColorUtils(unittest.TestCase):
     def setUpClass(cls):
         maya.standalone.initialize()  # Start Maya Headless (mayapy.exe)
 
-    def test_color_func(self):
+    def test_set_color_override_viewport(self):
+        test_obj = 'test_cube'
+        cmds.polyCube(name=test_obj)
 
-        # Assert Results - Fail
-        expected = ""
-        result = ""
-        self.assertEqual(result, expected)
+        expected = (0, 0.5, 1)
+        result = color_utils.set_color_override_viewport(test_obj, rgb_color=expected)
+        self.assertEqual(expected, result)
+
+    def test_set_color_override_outliner(self):
+        test_obj = 'test_cube'
+        cmds.polyCube(name=test_obj)
+
+        expected = (0, 0.5, 1)
+        result = color_utils.set_color_override_outliner(test_obj, rgb_color=expected)
+        self.assertEqual(expected, result)
--- utils/color_utils.py
+++ utils/color_utils.py
@@ -1,45 +1,54 @@
+"""
+Color Utilities
+github.com/TrevisanGMW/gt-tools
+"""
 import maya.cmds as cmds
 import logging
 import math
 
 # Logging Setup
 logging.basicConfig()
-logger = logging.getLogger("attribute_utils")
+logger = logging.getLogger("color_utils")
 logger.setLevel(logging.INFO)
 
 
-def change_viewport_color(obj, rgb_color=(1, 1, 1)):
+def set_color_override_viewport(obj, rgb_color=(1, 1, 1)):
     """
     Sets the outliner color for the selected object
 
     Args:
         obj (str): Name (path) of the object to change.
         rgb_color (tuple) : A tuple of 3 floats, RGB values. e.g. Red = (1, 0, 0)
-
+    Returns:
+        Set color if operation was successful. None if it failed
     """
-    if cmds.objExists(obj) and cmds.getAttr(obj + '.overrideEnabled', lock=True) is False:
-        cmds.setAttr(obj + '.overrideEnabled', 1)
-        cmds.setAttr(obj + '.overrideRGBColors', 1)
-        cmds.setAttr(obj + '.overrideColorRGB', rgb_color[0], rgb_color[1], rgb_color[2])
+    if cmds.objExists(obj) and cmds.getAttr(f'{obj}.overrideEnabled', lock=True) is False:
+        cmds.setAttr(f'{obj}.overrideEnabled', 1)
+        cmds.setAttr(f'{obj}.overrideRGBColors', 1)
+        cmds.setAttr(f'{obj}.overrideColorRGB', rgb_color[0], rgb_color[1], rgb_color[2])
+        set_color = cmds.getAttr(f'{obj}.overrideColorRGB') or []
+        if set_color and len(set_color) > 0:
+            return set_color[0]
 
 
-def set_color_outliner(obj_to_set):
+def set_color_override_outliner(obj, rgb_color=(1, 1, 1)):
     """
     Sets the outliner color for the selected object
 
     Args:
-        obj_to_set (str): Name (path) of the object to affect.
-
+        obj (str): Name of the object to change.
+        rgb_color (tuple) : A tuple of 3 floats, RGB values. e.g. Red = (1, 0, 0) - Range 0 to 1
+    Returns:
+        Set color if operation was successful. None if it failed
     """
-    extracted_r = math.pow(color[0], 0.454)
-    extracted_g = math.pow(color[1], 0.454)
-    extracted_b = math.pow(color[2], 0.454)
-
-    cmds.setAttr(obj_to_set + '.useOutlinerColor', 1)
-    cmds.setAttr(obj_to_set + '.outlinerColorR', extracted_r)
-    cmds.setAttr(obj_to_set + '.outlinerColorG', extracted_g)
-    cmds.setAttr(obj_to_set + '.outlinerColorB', extracted_b)
-    return True
+    if cmds.objExists(obj) and cmds.getAttr(f'{obj}.useOutlinerColor', lock=True) is False:
+        cmds.setAttr(f'{obj}.useOutlinerColor', 1)
+        cmds.setAttr(f'{obj}.outlinerColorR', rgb_color[0])
+        cmds.setAttr(f'{obj}.outlinerColorG', rgb_color[1])
+        cmds.setAttr(f'{obj}.outlinerColorB', rgb_color[2])
+        set_color = cmds.getAttr(f'{obj}.outlinerColor') or []
+        if set_color and len(set_color) > 0:
+            return set_color[0]
 
 
 if __name__ == "__main__":
