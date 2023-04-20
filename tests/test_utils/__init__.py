888ccd041c0870daadf4b16e8d360793e851748f
Guilherme Trevisan <trevisangmw@gmail.com>
2023-04-20 16:30:49 -0700

    Added new tests

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -1,4 +1,6 @@
 from . import test_alembic_utils
 from . import test_color_utils
+from . import test_list_utils
+from . import test_namespace_utils
 from . import test_session_utils
 from . import test_string_utils
