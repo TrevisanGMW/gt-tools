c417c2cdedaa6bfb5dfc3a5160f73a546173bd56
Guilherme Trevisan <trevisangmw@gmail.com>
2023-04-25 09:39:07 -0700

    Added options to launch Maya with python startup script

--- utils/system_utils.py
+++ utils/system_utils.py
@@ -3,6 +3,7 @@ System Utilities - Utilities related to system activities, such as paths, open e
 This script should not import "maya.cmds" as it's also intended to be used outside of Maya.
 github.com/TrevisanGMW/gt-tools
 """
+import base64
 import subprocess
 import tempfile
 import logging
@@ -88,19 +89,19 @@ def get_maya_install_dir(system):
     return autodesk_default_paths.get(system)
 
 
-def get_maya_path(system, version, maya_py_instead=False):
+def get_maya_path(system, version, get_maya_python=False):
     """
     Get a path to Maya executable or maya headless
     Args
         system (string): System name
         version (string): Software version - #### e.g. "2023" or "2024"
-        maya_py_instead (optional, bool): If active, it will return maya python executable instead of maya interactive
+        get_maya_python (optional, bool): If active, it will return maya python executable instead of maya interactive
     Returns:
         Path to Maya interactive or headless
     """
     install_dir = get_maya_install_dir(system)
     executable_name = "maya"
-    if maya_py_instead:
+    if get_maya_python:
         executable_name = "mayapy"
     maya_paths = {
         OS_LINUX: "/usr/bin/",
@@ -211,11 +212,15 @@ def get_available_maya_install_dirs():
         return {}
 
 
-def get_latest_maya_executable(get_maya_py_instead=False):
+def get_maya_executable(get_maya_python=False, preferred_version=None):
     """
     Gets a path to the executable of the latest available version of Maya detected on the system.
+    If a preferred version si provided and is available, that is used instead
     Args:
-        get_maya_py_instead (optional, bool): If active, it will attempt to retrieve "mayapy" instead of "maya"
+        get_maya_python (optional, bool): If active, it will attempt to retrieve "mayapy" instead of "maya"
+        preferred_version (optional, string): The preferred version. A string with four digits e.g. "2024"
+                                              If found, that will be used, otherwise the latest detected version
+                                              is returned instead.
     Returns:
         Path to Maya executable
         e.g. "C:\\Program Files\\Autodesk\\Maya2024\\bin\\maya.exe"
@@ -224,32 +229,83 @@ def get_latest_maya_executable(get_maya_py_instead=False):
     if len(maya_installs) == 0:
         logger.warning("Unable to find latest Maya path. No Maya installation detected on this system.")
         return
-    latest_maya_install_dir = get_maya_path(get_system(), max(maya_installs))
-    if not os.path.exists(latest_maya_install_dir):
-        logger.warning(f"Unable to find latest Maya path. Missing: {latest_maya_install_dir}")
+    maya_install_dir = get_maya_path(get_system(), max(maya_installs), get_maya_python=get_maya_python)
+    if preferred_version is not None and preferred_version in maya_installs:
+        maya_install_dir = get_maya_path(get_system(), preferred_version, get_maya_python=get_maya_python)
+    if not os.path.exists(maya_install_dir):
+        logger.warning(f"Unable to find latest Maya path. Missing: {maya_install_dir}")
         return
-    return latest_maya_install_dir
+    return maya_install_dir
 
 
-def launch_maya_from_path(maya_path, add_python_three_args=False):
+def launch_maya_from_path(maya_path, python_script=None, additional_args=None):
     """
-    Launches Maya
+    Launches Maya using provided path
     Args:
-        maya_path (string): Path to the maya executable e.g. "maya.exe"
-        add_python_three_args (optional, bool): If active, it will try to add arguments to tell maya to run python 3
+        maya_path (string): Path to the maya executable e.g. "maya.exe" (Complete path)
+        python_script (string): A python script in string format. If provided, it runs after opening Maya
+        additional_args (optional, list): Additional arguments. e.g. ["-pythonver", "3"]
+    Example:
+        launch_maya(maya_path="C:\\Program Files\\Autodesk\\Maya2024\\bin\\maya.exe"
+                    python_script='import sys; print("The arg number is: " + sys.argv[-1])',
+                    preferred_version='2024',
+                    additional_args=[7])  # Maya 2024 opens and prints "The arg number is: 7"
     """
+    if not os.path.exists(maya_path):
+        logger.warning(f"Unable to launch Maya. Provided path does not exist: {maya_path}")
+        return
+    args = [maya_path]
+    if python_script is not None:
+        encoded_python = base64.b64encode(python_script.encode('utf-8'))
+        script_text = '''python("import base64; exec (base64.urlsafe_b64decode({}))")'''
+        args += ['-c', script_text.format(encoded_python)]
+    # Additional Arguments
+    if additional_args is not None:
+        if isinstance(additional_args, list):
+            for arg in additional_args:
+                args.append(str(arg))
+        else:
+            logger.warning(f"Unable to use additional arguments. Please use a list.")
+    subprocess.check_call(args)
 
-    if add_python_three_args:
-        subprocess.Popen([maya_path, "-pythonver", "3"])
-    else:
-        subprocess.Popen([maya_path])
 
-
-def launch_latest_maya(add_python_three_args=False):
-    maya_path = get_latest_maya_executable()
+def launch_maya(preferred_version=None, python_script=None, additional_args=None):
+    """
+    Launches Maya latest automatically detected Maya executable
+    Args:
+        python_script (string): A python script in string format. If provided, it runs after opening Maya
+        preferred_version (optional, string): The preferred version. A string with four digits e.g. "2024"
+                                              If found, that will be used, otherwise the latest detected version
+                                              is returned instead.
+        additional_args (optional, list): A list of additional arguments (elements are converted to string)
+                                          e.g. ["-pythonver", "3"]
+    Example:
+        launch_maya(python_script='import sys; print("The arg number is: " + sys.argv[-1])',
+                    preferred_version='2024',
+                    additional_args=[7])  # Maya 2024 opens and prints "The arg number is: 7"
+    """
+    maya_path = get_maya_executable(preferred_version=preferred_version)
     if maya_path:
         launch_maya_from_path(maya_path=maya_path,
-                              add_python_three_args=add_python_three_args)
+                              python_script=python_script,
+                              additional_args=additional_args)
+
+
+def run_script_using_maya_python(script_path, preferred_version=None):
+    """
+    Runs provided script using the latest detected Maya Python ("mayapy")
+    Args:
+        script_path (string): Path to python script
+        preferred_version (optional, string): The preferred version. A string with four digits e.g. "2024"
+                                              If found, that will be used, otherwise the latest detected version
+                                              is returned instead.
+    """
+    headless_maya = get_maya_executable(get_maya_python=True, preferred_version=preferred_version)
+    if os.path.exists(headless_maya):
+        output = subprocess.call([str(headless_maya), script_path])
+        return output
+    else:
+        logger.warning(f"Unable to find maya python. Missing file: ")
 
 
 if __name__ == "__main__":
