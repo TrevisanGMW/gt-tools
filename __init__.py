786c29e8d646b962f930b2a1d437a2c8813a0c8e
Guilherme Trevisan <trevisangmw@gmail.com>
2023-04-29 13:55:58 -0700

    Updated version string

--- __init__.py
+++ __init__.py
@@ -8,9 +8,9 @@ PACKAGE_VERSION = "3.0.0"
 # Dependencies
 if sys.version_info.major < 3:
     # String formatting for this error should remain compatible to Python 2 to guarantee feedback
-    user_version = str(sys.version_info.major) + "." + str(sys.version_info.minor) + "." + str(sys.version_info.micro)
+    user_version = "{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
     error = "Incompatible Python Version. Expected to find 3+. Found version: " + user_version + "\n"
-    error += "For Python 2, use previous major version of GT-Tools (e.g 2.0.0+)\n"
+    error += "For Python 2, use an older version of GT-Tools before (e.g. 2.0.0)\n"
     sys.stdout.write(error)
     raise ImportError(error)
 
