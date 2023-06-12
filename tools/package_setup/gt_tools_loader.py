dc5f1c66298f607dc07a2d1b4e7a6cfae0020ae7
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-12 16:50:16 -0700

    Updated loader to check package dir

--- tools/package_setup/gt_tools_loader.py
+++ tools/package_setup/gt_tools_loader.py
@@ -14,17 +14,19 @@ logger = logging.getLogger("gt_tools_loader")
 logger.setLevel(logging.INFO)
 
 
-def import_package():
+def load_package():
     maya_preferences = os.path.dirname(cmds.about(preferences=True))
     package_path = os.path.join(maya_preferences, "gt-tools")
+    if not os.path.isdir(package_path):
+        logger.warning(f"Missing installation files. Expected location: {str(package_path)}")
+        return
     if package_path not in sys.path:
         sys.path.append(package_path)
     try:
         from ui import maya_menu
         maya_menu.load_menu()
     except Exception as e:
-        logger.warning(f"Expected path: {str(package_path)}")
         logger.warning(f"Unable to load GT Tools. Issue: {str(e)}")
 
 
-utils.executeDeferred(import_package)
+utils.executeDeferred(load_package)
