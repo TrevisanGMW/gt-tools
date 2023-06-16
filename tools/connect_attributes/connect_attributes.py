74738a52553cb342955dad307092d88b3d68d262
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-16 11:36:42 -0700

    Added launch function and updated changelog

--- tools/connect_attributes/connect_attributes.py
+++ tools/connect_attributes/connect_attributes.py
@@ -1,36 +1,6 @@
 """
  GT Connect Attributes Script
  github.com/TrevisanGMW/gt-tools - 2020-02-04
- 1.2 - 2020-02-18 
- Added force connection and some checks.
- 
- 1.3 - 2020-06-07 
- Updated naming convention to make it clearer. (PEP8)
- Fixed random window widthHeight issue.
- 
- 1.4 - 2020-06-17
- Added window icon
- Added help menu
- Changed GUI
- Attribute Listing now exported to txt file instead of script editor
- 
- 1.5 - 2020-11-15
- Tweaked the title color and text
- Tweaked a few colors
- 
- 1.6 - 2021-05-11
- Made script compatible with Python 3 (Maya 2022+)
- 
- 1.7 - 2021-08-22
- Fixed issue where default state for "use selection for source" would cause script to crash
-
- 1.8.0 - 2022-07-07
- Added patch to version
- PEP8 cleanup
-
- 1.8.1 - 2022-07-21
- PEP8 cleanup
-
 """
 import maya.cmds as cmds
 import maya.mel as mel
