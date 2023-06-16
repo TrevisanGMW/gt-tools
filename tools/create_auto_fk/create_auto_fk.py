d49940d02d7e2f67b4e1992edd1fbef077fcb667
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-16 11:36:46 -0700

    Added launch function and updated changelog

--- tools/create_auto_fk/create_auto_fk.py
+++ tools/create_auto_fk/create_auto_fk.py
@@ -1,51 +1,6 @@
 """
- GT Auto FK - Creates FK controls and move them to expected hierarchy.
+ GT Auto FK - Creates FK controls while mimicking joint hierarchy.
  github.com/TrevisanGMW/gt-tools - 2020-01-03
- 
- 1.2 - 2020-05-10 
- Fixed an issue where not using a suffix wouldn't build the hierarchy.
- 
- 1.3 - 2020-06-06 
- Added CtrlGrp, Ctrl, Jnt suffix text fields. 
- Removed joint length button.
- Fixed random window widthHeight issue.
- Updated naming convention to make it clearer. (PEP8)
- 
- 1.4 - 2020-06-17
- Added help button
- Changed GUI
- Added radius option
- Added icon
- Fixed offset bug on custom python curve
- 
- 1.5 - 2020-08-06
- Added persistent settings
- Fixed minor bugs
- Added a better error handling system
- Changed settings management to a dictionary
- Made the color for the custom curve button update when used
- 
- 1.6 - 2020-08-24
- Added undo chunk for main fk portion
- 
- 1.7 - 2020-11-15
- Tweaked the color and text for the title and help menu
- 
- 1.8 - 2021-05-11
- Made script compatible with Python 3 (Maya 2022+)
-
- 1.9.0 - 2022-06-27
- Added cube as an option for the custom curve dialog
-
- 1.9.1 - 2022-06-27
- Replace "Tag" with "Suffix" for the UI text
- A bit of clean up in the code
-
- 1.9.2 - 2022-06-29
- Added logger
- Added pin as an option for the custom curve dialog
- Small clean up to the code and docs
-
 """
 import maya.cmds as cmds
 import logging
