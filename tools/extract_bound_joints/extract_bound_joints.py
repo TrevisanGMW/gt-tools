fb6cd85d5d4e5bc645da953fdd9e4fb585743ed0
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-16 11:42:13 -0700

    Added launch function and updated changelog

--- tools/extract_bound_joints/extract_bound_joints.py
+++ tools/extract_bound_joints/extract_bound_joints.py
@@ -1,46 +1,6 @@
 """
 GT Extract Bound Joints - Extract or Transfer bound joints
 github.com/TrevisanGMW/gt-tools - 2022-06-22
-
-0.0.1 - 2022-06-22
-Core function
-
-0.0.2 - 2022-07-14
-Added GUI
-
-0.0.3 - 2022-07-20
-Added skinCluster check
-
-1.0.0 - 2022-07-20
-Added Filter non-existent and include mesh checkboxes
-
-1.0.1 - 2022-07-20
-Updated help menu
-
-1.0.2 - 2022-07-22
-Increased the size of the output window
-
-1.1.0 - 2022-07-26
-Added "Save to Shelf" button
-Added "Extract Bound Joints to Selection Sets" button
-
-1.1.1 - 2022-07-26
-Updated help
-Tweaked the UI spacing
-
-1.1.2 - 2022-07-26
-Fixed a typo
-Removed unnecessary parameter
-
-1.1.3 to 1.1.4 - 2022-09-12
-Changed "Include Bound Mesh" to be be inactive by default
-Added option to run bind/unbind skin functions
-Centered Checkbox options a bit better
-Fixed a few issues caused by the latest changes
-
-Todo:
-    Add Transfer functions
-    Add option to include maya.cmds
 """
 from maya import OpenMayaUI as OpenMayaUI
 import maya.cmds as cmds
