d5672d17da34b5e6e0c8ca21fd3feef36306222d
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-16 11:36:55 -0700

    Added launch function and updated changelog

--- tools/create_testing_keys/create_testing_keys.py
+++ tools/create_testing_keys/create_testing_keys.py
@@ -1,30 +1,6 @@
 """
-
  GT Create Testing Keys - Script for creating testing keyframes.
  github.com/TrevisanGMW/gt-tools -  2021-01-28
- 
- It creates a sequence of keyframes on the selected objects using the provided offset. 
- Helpful for when testing controls or painting skin weights.
- 
- 1.0 - 2021-01-28
- Initial Release
- 
- 1.1 - 2021-01-29
- Changed way that attributes are updated to account for long hierarchies (changed to setAttr instead of move/xform)
- Added a missing undoInfo(openChunk) function that would break the undo queue
- Updated a few incorrect comments
- 
- 1.2 - 2021-05-12
- Made script compatible with Python 3 (Maya 2022+)
- 
- 1.3 - 2021-06-23
- Added option to use world space to get predictable movement with joints
-
- 1.3.1 - 2022-07-21
- Added patch to version
- PEP8 Cleanup
- Added logger
-
 """
 import maya.cmds as cmds
 import logging
