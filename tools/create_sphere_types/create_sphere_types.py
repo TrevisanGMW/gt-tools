4c1ef92eb5151ae94fd1e61f724a1203f3bbf5d4
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-16 11:36:51 -0700

    Added launch function and updated changelog

--- tools/create_sphere_types/create_sphere_types.py
+++ tools/create_sphere_types/create_sphere_types.py
@@ -3,30 +3,6 @@
  standard sphere for everything.
  github.com/TrevisanGMW/gt-tools -  2020-11-04
  Tested on Maya 2020 - Windows 10
- 
- 1.1 - 2020-11-22
- Minor changes to the UI
- 
- 1.2 - 2020-12-03
- Platonic Sphere A is now created with soft normals
- 
- 1.3 - 2021-01-25
- Adjusted the size of the spacing between buttons
- 
- 1.3.1 - 2021-05-12
- Made script compatible with Python 3 (Maya 2022+)
- 
- 1.3.2 - 2021-06-22
- Fixed a little inconsistency on the size of the window
-
- 1.3.3 - 2022-07-10
- PEP8 Cleanup
-
- To do:
- Improve generated window to give better feedback
- Add more sphere options
- Add sliders to control subdivision level
- 
 """
 try:
     from shiboken2 import wrapInstance
