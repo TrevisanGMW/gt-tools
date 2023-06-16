0acda8e21e85aaf110a6811b33f91d3feef5d112
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-16 11:26:49 -0700

    Added launch function and updated changelog

--- tools/color_manager/color_manager.py
+++ tools/color_manager/color_manager.py
@@ -1,24 +1,6 @@
 """
  GT Color Manager - A script for managing the color of many objects at the same time (outliner and other overrides)
  github.com/TrevisanGMW/gt-tools - 2020-11-13
- 
- 1.1 - 2020-11-16
- Fixed an issue where the color containing rendering space data would be applied to the outliner.
- 
- 1.2 - 2020-11-23
- Fixed an issue with the persistent settings not being updated when importing the script.
- 
- 1.3 - 2020-12-03
- Fixed an issue where shape nodes wouldn't reset properly
- 
- 1.4 - 2021-05-11
- Made script compatible with Python 3 (Maya 2022+)
-
- 1.5.0 - 2022-07-07
- Added logging
- PEP8 Cleanup
- Added patch to version
- 
 """
 import maya.cmds as cmds
 import logging
