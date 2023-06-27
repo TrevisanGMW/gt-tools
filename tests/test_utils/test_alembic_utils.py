e3ee92b0205c9a12c07d7d6574eb8b9a534157ee
Guilherme Trevisan <trevisangmw@gmail.com>
2023-06-27 10:46:19 -0700

    Updated cmds to maya test tools

--- tests/test_utils/test_alembic_utils.py
+++ tests/test_utils/test_alembic_utils.py
@@ -8,36 +8,31 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Import Test Alembic Utilities
-tools_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-if tools_root_dir not in sys.path:
-    sys.path.append(tools_root_dir)
+# Import Tested Utility and Maya Test Tools
+test_utils_dir = os.path.dirname(__file__)
+tests_dir = os.path.dirname(test_utils_dir)
+package_root_dir = os.path.dirname(tests_dir)
+for to_append in [package_root_dir, tests_dir]:
+    if to_append not in sys.path:
+        sys.path.append(to_append)
+from tests import maya_test_tools
 from utils import alembic_utils
-
-try:
-    import maya.cmds as cmds
-    import maya.standalone
-    import maya.mel as mel
-    import maya.OpenMaya as OpenMaya
-except Exception as e:
-    logger.debug(str(e))
-    logger.warning("Unable load maya cmds, maya standalone, mel or OpenMaya")
 
 
 class TestSessionUtils(unittest.TestCase):
     def setUp(self):
-        cmds.file(new=True, force=True)
+        maya_test_tools.force_new_scene()
 
     @classmethod
     def setUpClass(cls):
-        maya.standalone.initialize()  # Start Maya Headless (mayapy.exe)
+        maya_test_tools.import_maya_standalone(initialize=True)  # Start Maya Headless (mayapy.exe)
 
     def test_load_alembic_plugin(self):
         alembic_utils.load_alembic_plugin(include_alembic_bullet=False)  # Default value
         result = []
         plugins_to_check = ["AbcExport", "AbcImport", "AbcBullet"]
         for plugin in plugins_to_check:
-            result.append(cmds.pluginInfo(plugin, q=True, loaded=True))
+            result.append(maya_test_tools.is_plugin_loaded(plugin))
         expected = [True, True, False]
         self.assertEqual(result, expected)
 
@@ -46,44 +41,44 @@ class TestSessionUtils(unittest.TestCase):
         result = []
         plugins_to_check = ["AbcExport", "AbcImport", "AbcBullet"]
         for plugin in plugins_to_check:
-            result.append(cmds.pluginInfo(plugin, q=True, loaded=True))
+            result.append(maya_test_tools.is_plugin_loaded(plugin))
         expected = [True, True, True]
         self.assertEqual(result, expected)
 
     def test_get_alembic_nodes(self):
         alembic_utils.load_alembic_plugin()  # Make sure alembic nodes can be created
-        alembic_node = cmds.createNode("AlembicNode")
+        alembic_node = maya_test_tools.create_node(node_type="AlembicNode")
         result = alembic_utils.get_alembic_nodes()
         expected = [alembic_node]
         self.assertEqual(result, expected)
 
     def test_get_alembic_nodes_two(self):
         alembic_utils.load_alembic_plugin()  # Make sure alembic nodes can be created
-        alembic_node_a = cmds.createNode("AlembicNode")
-        alembic_node_b = cmds.createNode("AlembicNode")
+        alembic_node_a = maya_test_tools.create_node(node_type="AlembicNode")
+        alembic_node_b = maya_test_tools.create_node(node_type="AlembicNode")
         result = alembic_utils.get_alembic_nodes()
         expected = [alembic_node_a, alembic_node_b]
         self.assertEqual(result, expected)
 
     def test_get_alembic_cycle_as_string(self):
         alembic_utils.load_alembic_plugin()  # Make sure alembic nodes can be created
-        alembic_node = cmds.createNode("AlembicNode")  # Default 0 = Hold
+        alembic_node = maya_test_tools.create_node("AlembicNode")  # Default 0 = Hold
         result = alembic_utils.get_alembic_cycle_as_string(alembic_node)  # Make sure alembic nodes can be created
         expected = 'Hold'
         self.assertEqual(result, expected)
 
     def test_get_alembic_cycle_as_string_reverse(self):
         alembic_utils.load_alembic_plugin()  # Make sure alembic nodes can be created
-        alembic_node = cmds.createNode("AlembicNode")  # Default 0 = Hold
-        cmds.setAttr(f'{alembic_node}.cycleType', 2)  # Change to 2 = Reverse
+        alembic_node = maya_test_tools.create_node(node_type="AlembicNode")  # Default 0 = Hold
+        maya_test_tools.set_attribute(obj_name=alembic_node, attr_name="cycleType", value=2)  # Change to 2 = Reverse
         result = alembic_utils.get_alembic_cycle_as_string(alembic_node)  # Make sure alembic nodes can be created
         expected = 'Reverse'
         self.assertEqual(result, expected)
 
     def test_alembic_node(self):
         alembic_utils.load_alembic_plugin()  # Make sure alembic nodes can be created
-        alembic_node = cmds.createNode("AlembicNode")  # Default 0 = Hold
-        cmds.setAttr(f'{alembic_node}.cycleType', 2)  # Change to 2 = Reverse
+        alembic_node = maya_test_tools.create_node(node_type="AlembicNode")  # Default 0 = Hold
+        maya_test_tools.set_attribute(obj_name=alembic_node, attr_name="cycleType", value=2)  # Change to 2 = Reverse
         result = alembic_utils.get_alembic_cycle_as_string(alembic_node)  # Make sure alembic nodes can be created
         expected = 'Reverse'
         self.assertEqual(result, expected)
--- tests/test_utils/test_color_utils.py
+++ tests/test_utils/test_color_utils.py
@@ -8,28 +8,28 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Import Test Session Utilities and Maya Test Tools
+# Import Tested Utility and Maya Test Tools
 test_utils_dir = os.path.dirname(__file__)
 tests_dir = os.path.dirname(test_utils_dir)
 package_root_dir = os.path.dirname(tests_dir)
 for to_append in [package_root_dir, tests_dir]:
     if to_append not in sys.path:
         sys.path.append(to_append)
-from tests.maya_test_tools import maya_test_utils
+from tests import maya_test_tools
 from utils import color_utils
 
 
 class TestColorUtils(unittest.TestCase):
     def setUp(self):
-        maya_test_utils.force_new_scene()
+        maya_test_tools.force_new_scene()
 
     @classmethod
     def setUpClass(cls):
-        maya_test_utils.import_maya_standalone()  # Start Maya Headless (mayapy.exe)
+        maya_test_tools.import_maya_standalone(initialize=True)  # Start Maya Headless (mayapy.exe)
 
     def test_set_color_override_viewport(self):
         test_obj = 'test_cube'
-        maya_test_utils.create_poly_cube(name=test_obj)
+        maya_test_tools.create_poly_cube(name=test_obj)
 
         expected = (0, 0.5, 1)
         result = color_utils.set_color_override_viewport(test_obj, rgb_color=expected)
@@ -37,7 +37,7 @@ class TestColorUtils(unittest.TestCase):
 
     def test_set_color_override_outliner(self):
         test_obj = 'test_cube'
-        maya_test_utils.create_poly_cube(name=test_obj)
+        maya_test_tools.create_poly_cube(name=test_obj)
 
         expected = (0, 0.5, 1)
         result = color_utils.set_color_override_outliner(test_obj, rgb_color=expected)
