89aa23eec8164bd32f64ab28160cfba5985a9434
TrevisanGMW <TrevisanGMW@gmail.com>
2023-07-01 16:32:41 -0700

    Created initial test_system_utils

---
+++ tests/test_utils/test_system_utils.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pathlib
+import logging
+import unittest
+import tempfile
+from unittest.mock import patch
+
+# Logging Setup
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Import Tested Utility and Maya Test Tools
+test_utils_dir = os.path.dirname(__file__)
+tests_dir = os.path.dirname(test_utils_dir)
+package_root_dir = os.path.dirname(tests_dir)
+for to_append in [package_root_dir, tests_dir]:
+    if to_append not in sys.path:
+        sys.path.append(to_append)
+from utils import system_utils
+
+
+class TestSystemUtils(unittest.TestCase):
+    def test_get_system(self):
+        result = system_utils.get_system()
+        expected = sys.platform
+        self.assertEqual(result, expected)
+
+    def test_get_temp_folder(self):
+        result = system_utils.get_temp_folder()
+        expected = tempfile.gettempdir()
+        self.assertEqual(result, expected)
+
+    @patch('system_utils.get_system')
+    def test_get_home_dir(self, mock_get_system):
+        mock_get_system.return_value = "random_value"
+        result = system_utils.get_home_dir()
+        expected = pathlib.Path.home()
+        self.assertEqual(expected, result)
+
+    @patch('system_utils.get_home_dir')
+    def test_get_desktop_path(self, mock_get_home_dir):
+        mock_get_home_dir.return_value = "path"
+        result = system_utils.get_desktop_path()
+        expected = os.path.join("paths", 'Desktop')
+        self.assertEqual(expected, result)
+
+
