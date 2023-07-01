77b13bb2738eaa8a447ce69962a3811c12e19bfd
TrevisanGMW <TrevisanGMW@gmail.com>
2023-07-01 09:38:34 -0700

    Added test_feedback_utils import

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -1,5 +1,6 @@
 from . import test_alembic_utils
 from . import test_color_utils
+from . import test_feedback_utils
 from . import test_list_utils
 from . import test_namespace_utils
 from . import test_scene_utils
