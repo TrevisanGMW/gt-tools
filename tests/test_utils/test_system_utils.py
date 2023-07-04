79531bb5351512f39e9f107303af14db2feaf037
Guilherme Trevisan <trevisangmw@gmail.com>
2023-07-03 21:37:14 -0700

    Fixed mocked paths not overwriting the correct functions

--- tests/test_utils/test_system_utils.py
+++ tests/test_utils/test_system_utils.py
@@ -4,7 +4,7 @@ import pathlib
 import logging
 import unittest
 import tempfile
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 # Logging Setup
 logging.basicConfig()
@@ -27,24 +27,27 @@ class TestSystemUtils(unittest.TestCase):
         expected = sys.platform
         self.assertEqual(expected, result)
 
+    @patch('sys.platform', 'mocked_platform')
+    def test_get_system_two(self):
+        result = system_utils.get_system()
+        expected = "mocked_platform"
+        self.assertEqual(expected, result)
+
     def test_get_temp_folder(self):
         result = system_utils.get_temp_folder()
         expected = tempfile.gettempdir()
         self.assertEqual(expected, result)
 
-    @patch('system_utils.get_system')
+    @patch('utils.system_utils.get_system')
     def test_get_home_dir(self, mock_get_system):
         mock_get_system.return_value = "random_value"
         result = system_utils.get_home_dir()
-        expected = str(pathlib.Path.home())
+        expected = pathlib.Path.home()  # Exactly what the function returns
         self.assertEqual(expected, result)
 
-    @patch('system_utils.get_home_dir')
+    @patch('utils.system_utils.get_home_dir')
     def test_get_desktop_path(self, mock_get_home_dir):
         mock_get_home_dir.return_value = "path"
         result = system_utils.get_desktop_path()
-        expected = ""
-        # expected = os.path.join("path", 'Desktop')
+        expected = os.path.join("path", "Desktop")
         self.assertEqual(expected, result)
-
-
