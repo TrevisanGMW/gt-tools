279ec534d2fbb32a1ac45ef0fbacc18d1a47cb09
Guilherme Trevisan <trevisangmw@gmail.com>
2023-07-09 15:03:21 -0700

    Made elements stretch

--- ui/progress_bar.py
+++ ui/progress_bar.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QApplication, QMainWindow, QProgressBar, QPushButton, QTextEdit
+from PySide2.QtWidgets import QApplication, QMainWindow, QProgressBar, QPushButton, QTextEdit, QVBoxLayout, QWidget
 from PySide2.QtGui import QIcon
 from ui import resource_library
 from ui import qt_utils
@@ -12,35 +12,37 @@ class ProgressBarWindow(QMainWindow):
 
         # Progress Bar
         self.progress_bar = QProgressBar(self)
-        self.progress_bar.setGeometry(50, 50, 400, 50)
         self.progress_bar.setTextVisible(False)
 
         # Output Window
         self.output_textbox = QTextEdit(self)
-        self.output_textbox.setGeometry(50, 120, 400, 200)
+        self.output_textbox.setReadOnly(True)
 
         # Temp Test Button
-        self.start_button = QPushButton("Start", self)
-        self.start_button.setGeometry(50, 340, 75, 25)
+        self.start_button = QPushButton("OK", self)
         self.start_button.clicked.connect(self.start_progress)
+
+        # Main layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.progress_bar)
+        layout.addWidget(self.output_textbox)
+        layout.addWidget(self.start_button)
+
+        # Central widget
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
         # Window
         _min_width = 500
         _min_height = 400
         self.setGeometry(0, 0, _min_width, _min_height)  # Args X, Y, W, H
-        self.setMinimumWidth(_min_width)
-        self.setMinimumHeight(_min_height)
-        # Window Adjust
-        self.adjustSize()
-        self.setMinimumWidth(self.width())
-        self.setMinimumHeight(self.height())
+        self.setMinimumWidth(300)
         self.center()
         # Window Details
-        self.setWindowTitle("Progress Bar Example")
+        self.setWindowTitle("Progress Bar")
         self.setStyleSheet(resource_library.Stylesheet.maya_progress_bar)
-        print(resource_library.Stylesheet.maya_progress_bar)
         self.setWindowIcon(QIcon(resource_library.Icon.package_icon))
-        self.start_progress()
 
     def center(self):
         """ Moves window to the center of the screen """
@@ -84,24 +86,19 @@ Copying required files...
 Adding entry point to userSetup...
 Checking installation integrity...
 
-Removing previous install...
-Copying required files...
-Adding entry point to userSetup...
-Checking installation integrity...
-
 Installation completed successfully!""")
         index = 0
-        # while index < 100:
-        #     # self.progress_bar.setValue(i)
-        #     increase_value = 5
-        #     self.increase_progress_value(increase_value)
-        #     index += increase_value
-        #     # self.append_text_to_output_box(f"Progress: {index}%")
-        #     QApplication.processEvents()  # Updates the GUI and keeps it responsive
-        #     # Simulate some work being done
-        #     # Replace this with your actual work
-        #     import time
-        #     time.sleep(0.1)
+        while index < 100:
+            # self.progress_bar.setValue(i)
+            increase_value = 5
+            self.increase_progress_value(increase_value)
+            index += increase_value
+            # self.append_text_to_output_box(f"Progress: {index}%")
+            QApplication.processEvents()  # Updates the GUI and keeps it responsive
+            # Simulate some work being done
+            # Replace this with your actual work
+            import time
+            time.sleep(0.1)
 
 
 if __name__ == '__main__':
