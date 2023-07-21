35542fa9d57a25cef162cdfe03680bc0e2c66d20
Guilherme Trevisan <trevisangmw@gmail.com>
2023-07-21 13:32:49 -0700

    Moved connections to controller

--- gt/tools/sample_tool/__init__.py
+++ gt/tools/sample_tool/__init__.py
@@ -35,23 +35,7 @@ def build_tool_example_gui(standalone=True):
     # Create connections
     _model = sample_model.SampleToolModel()
     _controller = sample_controller.SampleToolController(model=_model, view=_view)
-    # _view.controller = _controller  # To avoid garbage collection
-
-    # # Buttons
-    # _view.ButtonInstallClicked.connect(_controller.install_package)
-    # _view.ButtonUninstallClicked.connect(_controller.uninstall_package)
-    # _view.ButtonRunOnlyClicked.connect(_controller.run_only_package)
-    #
-    # # Feedback
-    # _controller.UpdatePath.connect(_view.update_installation_path_text_field)
-    # _controller.UpdateVersion.connect(_view.update_version_texts)
-    # _controller.UpdateStatus.connect(_view.update_status_text)
-    # _controller.CloseView.connect(_view.close_window)
-
-    # # Initial Update
-    # _controller.update_path()
-    # _controller.update_version()
-    # _controller.update_status()
+    _controller.update_view()
 
     # Show window
     if standalone:
