58ccfa83f18df326fab1cc23482bd72b0673b64b
Guilherme Trevisan <trevisangmw@gmail.com>
2023-07-27 14:57:09 -0700

    Added a few new icons

--- gt/ui/resource_library.py
+++ gt/ui/resource_library.py
@@ -264,8 +264,11 @@ class Icon:
     tool_retarget_assistant = get_icon_path(r"tool_retarget_assistant.svg")
     tool_game_fbx_exporter = get_icon_path(r"tool_game_fbx_exporter.svg")
     tool_influence_joints = get_icon_path(r"tool_influence_joints.svg")
+    tool_add_inbetween = get_icon_path(r"tool_add_inbetween.svg")
     tool_sine_attributes = get_icon_path(r"tool_sine_attributes.svg")
     tool_connect_attributes = get_icon_path(r"tool_connect_attributes.svg")
+    tool_create_fk = get_icon_path(r"tool_create_fk.svg")
+    tool_testing_keys = get_icon_path(r"tool_testing_keys.svg")
     # Misc
     tool_maya_to_discord = get_icon_path(r"tool_maya_to_discord.svg")
     tool_fspy_importer = get_icon_path(r"tool_fspy_importer.svg")
