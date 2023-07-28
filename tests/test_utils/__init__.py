8a9cdea41cf72a8fa5fc9efcc38a709c8b38e5fc
Guilherme Trevisan <trevisangmw@gmail.com>
2023-07-28 10:58:02 -0700

    Added prefs utils to all tests

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -5,6 +5,7 @@ from . import test_data_utils
 from . import test_feedback_utils
 from . import test_list_utils
 from . import test_namespace_utils
+from . import test_prefs_utils
 from . import test_scene_utils
 from . import test_session_utils
 from . import test_setup_utils
