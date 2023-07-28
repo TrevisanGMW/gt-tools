09060d59ab38c5af262e14f8652956ed4d6310a3
Guilherme Trevisan <trevisangmw@gmail.com>
2023-07-28 10:57:37 -0700

    Created prefs_utils

---
+++ gt/utils/prefs_utils.py
@@ -0,0 +1,224 @@
+"""
+Preferences Utilities - Settings and Getting persistent settings using JSONs
+This script should not directly import "maya.cmds" as it's also intended to be used outside of Maya.
+github.com/TrevisanGMW/gt-tools
+"""
+import shutil
+
+from gt.utils.system_utils import get_maya_preferences_dir, get_system
+from gt.utils.data_utils import write_json, read_json_dict
+from gt.utils.setup_utils import PACKAGE_NAME
+import logging
+import os
+
+# Logging Setup
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Constants
+PACKAGE_PREFS_DIR = "prefs"
+PACKAGE_PREFS_EXT = "json"
+
+
+class Prefs:
+    def __init__(self, filename, location_dir=None):
+        """
+        Initialize the Prefs class.
+
+        Args:
+            filename (str): The name of the file to save and load preferences.
+            location_dir (str, optional): Path to a folder where it should save the JSON file.
+                                          By default, preferences are saved in the package installation path.
+                                          e.g. "Documents/maya/gt-tools/prefs"
+        """
+        if location_dir:
+            if os.path.exists(location_dir) and os.path.isdir(location_dir):
+                self.filename = os.path.join(location_dir, f'{filename}.{PACKAGE_PREFS_EXT}')
+        else:
+            _maya_preferences_dir = get_maya_preferences_dir(get_system())
+            _package_parent_dir = os.path.join(_maya_preferences_dir, PACKAGE_NAME)
+            _prefs_dir = os.path.join(_package_parent_dir, PACKAGE_PREFS_DIR)
+            self.filename = os.path.join(_prefs_dir, f'{filename}.{PACKAGE_PREFS_EXT}')
+        self.preferences = {}
+        self.load()
+
+    # ------------------------------------ Getters ------------------------------------
+
+    def get_float(self, key, default=None):
+        """
+        Returns a float value corresponding to key in the preference file if it exists.
+        It also has a second argument for a default value, which is returned when the key doesn't exist.
+
+        Args:
+            key (str): The key to retrieve the float value from preferences.
+            default (float, optional): The default value to return when the key doesn't exist. Defaults to None.
+
+        Returns:
+            float: The float value associated with the key, or the default value if the key is not found.
+        """
+        return self.preferences.get(key, default)
+
+    def get_int(self, key, default=None):
+        """
+        Returns the value corresponding to key in the preference file if it exists.
+        It also has a second argument for a default value, which is returned when the key doesn't exist.
+
+        Args:
+            key (str): The key to retrieve the integer value from preferences.
+            default (int, optional): The default value to return when the key doesn't exist. Defaults to None.
+
+        Returns:
+            int: The integer value associated with the key, or the default value if the key is not found.
+        """
+        return self.preferences.get(key, default)
+
+    def get_string(self, key, default=None):
+        """
+        Returns the value corresponding to key in the preference file if it exists.
+        It also has a second argument for a default value, which is returned when the key doesn't exist.
+
+        Args:
+            key (str): The key to retrieve the string value from preferences.
+            default (str, optional): The default value to return when the key doesn't exist. Defaults to None.
+
+        Returns:
+            str: The string value associated with the key, or the default value if the key is not found.
+        """
+        return self.preferences.get(key, default)
+
+    def get_raw_json(self):
+        """
+        Returns the entire raw JSON data as a dictionary.
+
+        Returns:
+            dict: The raw JSON data as a dictionary.
+        """
+        return self.preferences
+
+    def has_key(self, key):
+        """
+        Returns True if the given key exists in Prefs, otherwise returns False.
+
+        Args:
+            key (str): The key to check for existence in preferences.
+
+        Returns:
+            bool: True if the key exists in preferences, False otherwise.
+        """
+        return key in self.preferences
+
+    # ------------------------------------ Setters ------------------------------------
+
+    def set_float(self, key, value):
+        """
+        Sets the float value of the preference identified by the given key.
+
+        Args:
+            key (str): The key to set the float value in preferences.
+            value (float): The float value to be set for the given key.
+        """
+        self.preferences[key] = float(value)
+
+    def set_int(self, key, value):
+        """
+        Sets a single integer value for the preference identified by the given key.
+
+        Args:
+            key (str): The key to set the integer value in preferences.
+            value (int): The integer value to be set for the given key.
+        """
+        self.preferences[key] = int(value)
+
+    def set_string(self, key, value):
+        """
+        Sets a single string value for the preference identified by the given key.
+
+        Args:
+            key (str): The key to set the string value in preferences.
+            value (str): The string value to be set for the given key.
+        """
+        self.preferences[key] = str(value)
+
+    # ------------------------------------ Load/Save ------------------------------------
+
+    def load(self):
+        """
+        Loads preferences from the JSON file if it exists.
+        """
+        if os.path.exists(self.filename):
+            self.preferences = read_json_dict(path=self.filename)
+
+    def save(self):
+        """
+        Saves all modified preferences to the JSON file.
+        """
+        _prefs_dir = os.path.dirname(self.filename)
+        if not os.path.isdir(_prefs_dir):
+            os.makedirs(_prefs_dir)
+            logger.debug(f'Prefs directory was missing and was created when saving: {_prefs_dir}')
+        write_json(path=self.filename, data=self.preferences)
+
+    # ------------------------------------ Utilities ------------------------------------
+
+    def delete_all(self):
+        """
+        Removes all keys and values from the preferences.
+        """
+        self.preferences = {}
+
+    def delete_key(self, key):
+        """
+        Removes the given key from the Prefs class.
+
+        Args:
+            key (str): The key to be deleted from preferences.
+        """
+        if key in self.preferences:
+            del self.preferences[key]
+        else:
+            logger.debug(f'Key "{key}" not found.')
+
+    def purge_preferences_dir(self, purge_preferences=False):
+        """
+        WARNING!!!! Be careful!!! This will delete all preferences files.
+        Purges the preferences' directory associated with the current filename.
+
+        This method deletes all preferences located in the directory containing the file specified by 'self.filename',
+        provided that the 'purge_preferences' flag is set to True.
+
+        Parameters:
+           purge_preferences (bool, optional): If True, the function will delete all preferences in the directory.
+               If False or not provided, the function will not perform any deletion.
+
+        Returns:
+           bool: True if the preferences' directory was purged successfully, False otherwise.
+
+        Note:
+           The preferences' directory will only be purged if both 'self.filename' points to an existing file and
+           'purge_preferences' is set to True. If 'self.filename' does not exist or 'purge_preferences' is False,
+           the function will do nothing and return False.
+
+        Example:
+           # Create an instance of the class
+           obj = SomeClass()
+
+           # Set the filename attribute
+           obj.filename = '/path/to/preferences/file.txt'
+
+           # Purge preferences from the directory
+           result = obj.purge_preferences_dir(purge_preferences=True)
+        """
+        _prefs_dir = os.path.dirname(self.filename)
+        if os.path.exists(_prefs_dir) and purge_preferences:
+            logger.debug(f'Purging all preferences from: "{_prefs_dir}"')
+            shutil.rmtree(_prefs_dir)
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
+    from pprint import pprint
+    out = None
+    pprint(out)
---
+++ tests/test_utils/test_prefs_utils.py
@@ -0,0 +1,80 @@
+import unittest
+import logging
+import sys
+import os
+
+# Logging Setup
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Import Tested Utility and Maya Test Tools
+test_utils_dir = os.path.dirname(__file__)
+tests_dir = os.path.dirname(test_utils_dir)
+package_root_dir = os.path.dirname(tests_dir)
+for to_append in [package_root_dir, tests_dir]:
+    if to_append not in sys.path:
+        sys.path.append(to_append)
+from tests import maya_test_tools
+from gt.utils import prefs_utils
+
+
+class TestPrefsUtils(unittest.TestCase):
+    def setUp(self):
+        self.mocked_str = "mocked_data"
+        self.mocked_dict = {"mocked_key_a": "mocked_value_a",
+                            "mocked_key_b": "mocked_value_b"}
+        self.temp_dir = maya_test_tools.generate_test_temp_dir()
+        self.prefs = prefs_utils.Prefs(filename="mock_prefs", location_dir=self.temp_dir)
+
+    def tearDown(self):
+        maya_test_tools.delete_test_temp_dir()
+
+    def test_set_and_get_float(self):
+        self.prefs = prefs_utils.Prefs("mock_prefs")
+        self.prefs.set_float('pi', 3.14159)
+        self.assertEqual(self.prefs.get_float('pi'), 3.14159)
+        self.assertEqual(self.prefs.get_float('not_found', 42.0), 42.0)
+
+    def test_set_and_get_int(self):
+        self.prefs.set_int('age', 25)
+        self.assertEqual(self.prefs.get_int('age'), 25)
+        self.assertEqual(self.prefs.get_int('not_found', 0), 0)
+
+    def test_set_and_get_string(self):
+        self.prefs.set_string('name', 'John Doe')
+        self.assertEqual(self.prefs.get_string('name'), 'John Doe')
+        self.assertEqual(self.prefs.get_string('not_found', 'Unknown'), 'Unknown')
+
+    def test_delete_key(self):
+        self.prefs.set_float('pi', 3.14159)
+        self.prefs.delete_key('pi')
+        self.assertFalse(self.prefs.has_key('pi'))
+
+    def test_delete_all(self):
+        self.prefs.set_float('pi', 3.14159)
+        self.prefs.set_int('age', 25)
+        self.prefs.set_string('name', 'John Doe')
+
+        self.prefs.delete_all()
+        self.assertFalse(self.prefs.has_key('pi'))
+        self.assertFalse(self.prefs.has_key('age'))
+        self.assertFalse(self.prefs.has_key('name'))
+
+    def test_get_raw_json(self):
+        self.prefs.set_float('pi', 3.14159)
+        self.prefs.set_int('age', 25)
+        self.prefs.set_string('name', 'John Doe')
+
+        raw_json_data = self.prefs.get_raw_json()
+        expected_json = {
+            'pi': 3.14159,
+            'age': 25,
+            'name': 'John Doe'
+        }
+        self.assertEqual(raw_json_data, expected_json)
+
+    def test_purge_prefs_folder(self):
+        self.assertTrue(os.path.exists(self.temp_dir))
+        self.prefs.purge_preferences_dir(purge_preferences=True)
+        self.assertFalse(os.path.exists(self.temp_dir))
