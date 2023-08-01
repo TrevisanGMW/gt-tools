1ae87b3bee88d3630f19aa0c1cf83e15cbb6cd09
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-01 16:20:57 -0700

    Added curve library to dev menu

--- gt/tools/package_setup/gt_tools_maya_menu.py
+++ gt/tools/package_setup/gt_tools_maya_menu.py
@@ -373,6 +373,10 @@ def load_menu(*args):
                            command=IMPORT_TOOL + 'initialize_tool("sample_tool")',
                            tooltip="Opens sample tool.",
                            icon=resource_library.Icon.dev_screwdriver)
+        menu.add_menu_item(label='Curve Library',
+                           command=IMPORT_TOOL + 'initialize_tool("curve_library")',
+                           tooltip="Opens sample tool.",
+                           icon=resource_library.Icon.dev_screwdriver)
         menu.add_divider()  # Curve Thumbnails Section +++++++++++++++++++++++++++++++++
         menu.add_menu_item(label='Add Thumbnail Metadata to Selection',
                            command='from gt.utils.curve_utils import add_thumbnail_metadata_attr_to_selection\n'
