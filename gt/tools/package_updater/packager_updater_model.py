5e72ddbabf2407bb787ff38287744a46801c7219
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-03 16:47:23 -0700

    Added prefs

--- gt/tools/package_updater/packager_updater_model.py
+++ gt/tools/package_updater/packager_updater_model.py
@@ -2,10 +2,10 @@
 Package Updater Model
 
 Classes:
-    PackageUpdaterModel: A class for managing a library of curves.
+    PackageUpdaterModel: A class for checking for updates
 """
-from gt.utils.curve_utils import Curves, get_curve_preview_image_path
-from gt.ui import resource_library
+from gt.utils.prefs_utils import Prefs
+from datetime import datetime
 import logging
 
 # Logging Setup
@@ -13,13 +13,43 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Constants
+PREFS_NAME = "package_updater"
+PREFS_LAST_DATE = "last_date"  # Format: '2020-01-01 17:08:00'
+PREFS_AUTO_CHECK = "auto_check"
+PREFS_INTERVAL_DAYS = "interval_days"
+PACKAGE_RELEASES_URL = 'https://github.com/TrevisanGMW/gt-tools/releases/latest'
+
 
 class PackageUpdaterModel:
     def __init__(self):
         """
         Initialize the PackageUpdaterModel object.
         """
-        self.curves = []
+        self.preferences = Prefs(PREFS_NAME)
+        today_date = datetime(datetime.now().year, datetime.now().month, datetime.now().day)
+        self.last_date = str(today_date)
+        self.auto_check = True
+        self.interval_days = 15
+        self.update_preferences()
+
+    def get_preferences(self):
+        return self.preferences.get_raw_preferences()
+
+    def update_preferences(self):
+        """
+        Updates preferences from installation folder. If nothing is found, it keeps previous values.
+        """
+        self.last_date = self.preferences.get_string(key=PREFS_LAST_DATE, default=self.last_date)
+        self.auto_check = self.preferences.get_bool(key=PREFS_AUTO_CHECK, default=self.auto_check)
+        self.interval_days = self.preferences.get_int(key=PREFS_INTERVAL_DAYS, default=self.interval_days)
+
+    def set_preferences(self):
+        """ Set preferences and save it to disk """
+        self.preferences.set_string(key=PREFS_LAST_DATE, value=str(self.last_date))
+        self.preferences.set_bool(key=PREFS_AUTO_CHECK, value=self.auto_check)
+        self.preferences.set_int(key=PREFS_INTERVAL_DAYS, value=self.interval_days)
+        self.preferences.save()
 
     def check_for_updates(self):
         pass
@@ -27,4 +57,5 @@ class PackageUpdaterModel:
 
 if __name__ == "__main__":
     model = PackageUpdaterModel()
-    print(model)
+    # model.set_preferences()
+    print(model.last_date)
