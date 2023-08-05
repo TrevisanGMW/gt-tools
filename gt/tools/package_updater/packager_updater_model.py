0348a6dcae6de383e2db66cdd6f4ed0419d08423
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-05 11:49:30 -0700

    Created get_releases_changelog

--- gt/tools/package_updater/packager_updater_model.py
+++ gt/tools/package_updater/packager_updater_model.py
@@ -4,9 +4,11 @@ Package Updater Model
 Classes:
     PackageUpdaterModel: A class for checking for updates
 """
+from gt.utils.string_utils import remove_prefix
 from gt.utils.prefs_utils import Prefs
 from gt.utils import version_utils
 from datetime import datetime
+from json import loads
 import threading
 import logging
 
@@ -35,12 +37,19 @@ class PackageUpdaterModel:
         self.update_preferences()
         # Status
         self.status = "Not Installed"
-        self.web_response = None
         self.installed_version = "0.0.0"
         self.latest_github_version = "0.0.0"
         self.needs_update = False
+        # Request Data
+        self.web_response = None
+        self.response_content = None
 
     def get_preferences(self):
+        """
+        Returns existing preferences in raw form (dictionary)
+        Returns:
+            dict: A dictionary with the current preferences.
+        """
         return self.preferences.get_raw_preferences()
 
     def update_preferences(self):
@@ -51,24 +60,12 @@ class PackageUpdaterModel:
         self.auto_check = self.preferences.get_bool(key=PREFS_AUTO_CHECK, default=self.auto_check)
         self.interval_days = self.preferences.get_int(key=PREFS_INTERVAL_DAYS, default=self.interval_days)
 
-    def set_preferences(self):
+    def save_preferences(self):
         """ Set preferences and save it to disk """
         self.preferences.set_string(key=PREFS_LAST_DATE, value=str(self.last_date))
         self.preferences.set_bool(key=PREFS_AUTO_CHECK, value=self.auto_check)
         self.preferences.set_int(key=PREFS_INTERVAL_DAYS, value=self.interval_days)
         self.preferences.save()
-
-    def check_for_updates(self):
-        """ Checks current version against web version and updates stored values with retrieved data """
-        # Current Version
-        self.installed_version = version_utils.get_installed_version()
-        # Latest Version
-        response, response_content = version_utils.get_github_releases()
-        self.latest_github_version = version_utils.get_latest_github_release_version(response_content=response_content)
-        # Status
-        self.refresh_status()
-        # Web Response
-        self.web_response = response.status
 
     def refresh_status(self):
         """ Updates status and  by comparing installed version with the latest GitHub version """
@@ -86,10 +83,65 @@ class PackageUpdaterModel:
             self.status = "You're up to date!"
             self.needs_update = False
 
+    def request_github_data(self):
+        """ Requests GitHub data and stores it """
+        response, response_content = version_utils.get_github_releases()
+        self.response_content = response_content
+        self.web_response = response.status
+
+    def check_for_updates(self):
+        """ Checks current version against web version and updates stored values with retrieved data """
+        # Current Version
+        self.installed_version = version_utils.get_installed_version()
+        # Latest Version
+        self.request_github_data()
+        response_content = self.response_content
+        self.latest_github_version = version_utils.get_latest_github_release_version(response_content=response_content)
+        # Status
+        self.refresh_status()
+
     def threaded_check_for_updates(self):
         """ Threaded Check for updates """
         thread = threading.Thread(None, target=self.check_for_updates)
         thread.start()
+
+    def get_releases_changelog(self, num_releases=3):
+        """
+        Creates a list of strings with the tag name, release date and body for the latest releases.
+        Useful for when presenting a list of changes to the user.
+
+        Args:
+            num_releases (int, optional): Number of releases to return. If the total is lower than the provided number,
+                                          that will be returned instead. For example: If 10 releases are found but this
+                                          variable is set to 3, then only the 3 latest releases will be returned.
+                                          If num_releases = 3, but only 2 were found, then 2 will be returned.
+        Returns:
+            list: A list of strings with tag name, release date and body.
+        """
+        content = None
+        changelog_data = []
+        if not self.response_content:
+            logger.warning("Unable to retrieve changelog. Request content is empty")
+            return
+        try:
+            content = loads(self.response_content)
+        except Exception as e:
+            logger.warning(f'Unable to interpret content data. Issue: "{str(e)}".')
+        if not content:
+            logger.warning("Unable to retrieve changelog. Request content is empty")
+            return
+        if isinstance(content, list) and len(content) >= num_releases:
+            content = content[:3]
+        for data in content:
+            text_line = ''
+            text_line += data.get('tag_name', '')
+            published_at = data.get('published_at', '').split('T')[0]
+            text_line += f' - ({published_at})\n'
+            body = data.get('body', '')
+            text_line += remove_prefix(body, "\r\n## What's Changed\r\n\r\n")
+            text_line += "\n"
+            changelog_data.append(text_line)
+        return changelog_data
 
 
 if __name__ == "__main__":
@@ -97,8 +149,9 @@ if __name__ == "__main__":
     import maya.standalone
     maya.standalone.initialize()
     model = PackageUpdaterModel()
-    # model.set_preferences()
-    # get_installed_version()
-    model.threaded_check_for_updates()
-
+    model.check_for_updates()
+    changelog = model.get_releases_changelog()
+    from pprint import pprint
+    print(changelog)
+    # print(model.__dict__)
 
