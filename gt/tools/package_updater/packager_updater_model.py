97c7a28ec3b865c55b0bed70f25bfb8743b1edb2
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-06 15:44:54 -0700

    Created update_package

--- gt/tools/package_updater/packager_updater_model.py
+++ gt/tools/package_updater/packager_updater_model.py
@@ -6,11 +6,11 @@ Classes:
 """
 from gt.utils.setup_utils import remove_package_loaded_modules
 from gt.utils.setup_utils import PACKAGE_MAIN_MODULE
+from gt.utils.prefs_utils import Prefs, PackageCache
 from gt.utils.request_utils import download_file
 from gt.utils.string_utils import remove_prefix
 from gt.utils.data_utils import unzip_zip_file
 from PySide2.QtWidgets import QApplication
-from gt.utils.prefs_utils import Prefs
 from gt.utils import feedback_utils
 from gt.utils import version_utils
 from gt.ui import resource_library
@@ -155,39 +155,60 @@ class PackageUpdaterModel:
             changelog_data.append(text_line)
         return changelog_data
 
-    def update_package(self):
-        # if not self.needs_update:
-        #     logger.debug("Package does not need to be updated.")
-        #     return
+    def update_package(self, cache=None, force_update=True):
+        """
+        Updates the package to the latest release found on GitHub.
+        Args:
+            cache (PackageCache, optional): If provided, it will be used to generate the cache.
+                                            If not, a PackageCache object will be created during installation.
+                                            The temporary cache will point to the default cache location:
+                                            "maya/gt-tools/cache" and will automatically be purged at the end.
+                                            If the operation crashes, It's not guaranteed that it will be purged.
+            force_update (bool, optional): If active, it will update even if the update is not necessary.
+        """
+        if not self.needs_update and not force_update:
+            logger.debug("Package does not need to be updated.")
+            return
 
-        # zip_file_url = None
-        # try:
-        #     content = loads(self.response_content)
-        #     if isinstance(content, list):
-        #         content = content[0]
-        #     zip_file_url = content.get('zipball_url')
-        #     if not zip_file_url:
-        #         raise Exception('Missing "zipball_url" value.')
-        # except Exception as e:
-        #     logger.warning(f'Unable to update. Failed to interpret content data. Issue: "{str(e)}".')
-        #     return
-        # print(zip_file_url)
-        #
-        # if not QApplication.instance():
-        #     app = QApplication(sys.argv)
-        #     print(app)
+        zip_file_url = None
+        try:
+            content = loads(self.response_content)
+            if isinstance(content, list):
+                content = content[0]
+            zip_file_url = content.get('zipball_url')
+            if not zip_file_url:
+                raise Exception('Missing "zipball_url" value.')
+        except Exception as e:
+            logger.warning(f'Unable to update. Failed to interpret content data. Issue: "{str(e)}".')
+            return
+
+        if not QApplication.instance():
+            app = QApplication(sys.argv)
 
         self.progress_win = progress_bar.ProgressBarWindow()
         self.progress_win.show()
         self.progress_win.set_progress_bar_name("Updating Script Package...")
+
         # Create connections
         self.progress_win.first_button.clicked.connect(self.progress_win.close_window)
         # Number of print functions inside installer (7) + download (1) + extract (1) + clean-up (1)
         self.progress_win.set_progress_bar_max_value(10)
         self.progress_win.increase_progress_bar_value()
-        zip_file_url = r'https://api.github.com/repos/TrevisanGMW/gt-tools/zipball/v3.0.5'
-        download_path = r'C:\Users\guilherme.trevisan\Desktop\target\sample_file.zip'
-        extract_path = r'C:\Users\guilherme.trevisan\Desktop\target\extract'
+
+        if cache and isinstance(cache, PackageCache):
+            _cache = cache
+        else:
+            _cache = PackageCache()
+        cache_dir = _cache.get_cache_dir()
+        cache_download = os.path.join(cache_dir, "package_update.zip")
+        cache_extract = os.path.join(cache_dir, "update_extract")
+        _cache.add_path_to_cache_list(cache_download)
+        _cache.add_path_to_cache_list(cache_extract)
+
+        if not os.path.exists(cache_dir):
+            message = f'Unable to create cache location. Update operation cancelled. Location: {cache_dir}'
+            self.progress_win.add_text_to_output_box(input_string=message, color=resource_library.Color.Hex.red_soft)
+            return
 
         # Download Update --------------------------------------------------
         self.progress_win.add_text_to_output_box("Downloading Update...")
@@ -200,7 +221,7 @@ class PackageUpdaterModel:
             self.progress_win.add_text_to_output_box(output_box, as_new_line=True)
 
         try:
-            download_file(url=zip_file_url, destination=download_path, chunk_size=65536,
+            download_file(url=zip_file_url, destination=cache_download, chunk_size=65536,
                           callback=print_download_progress)
             self.progress_win.increase_progress_bar_value()
         except Exception as e:
@@ -219,25 +240,25 @@ class PackageUpdaterModel:
             self.progress_win.add_text_to_output_box(output_box)
 
         try:
-            unzip_zip_file(zip_file_path=download_path, extract_path=extract_path, callback=print_extract_progress)
+            unzip_zip_file(zip_file_path=cache_download, extract_path=cache_extract, callback=print_extract_progress)
             self.progress_win.increase_progress_bar_value()
         except Exception as e:
             self.progress_win.add_text_to_output_box(input_string=str(e), color=resource_library.Color.Hex.red_soft)
             return
 
         # Validate Extraction ----------------------------------------------------
-        extracted_content = os.listdir(extract_path)  # TODO this must be purged to only have one folder
+        extracted_content = os.listdir(cache_extract)  # TODO this must be purged to only have one folder
         if not extracted_content:
-            message = f'Extraction returned no files.\nExtraction Path: "{extract_path}".'
+            message = f'Extraction returned no files.\nExtraction Path: "{cache_extract}".'
             self.progress_win.add_text_to_output_box(input_string=message, color=resource_library.Color.Hex.red_soft)
             return
         extracted_dir_name = extracted_content[0]
-        extracted_dir_path = os.path.join(extract_path, extracted_dir_name)
+        extracted_dir_path = os.path.join(cache_extract, extracted_dir_name)
         extracted_module_path = None
         if os.path.exists(extracted_dir_path) and os.path.isdir(extracted_dir_path):
             extracted_module_path = os.path.join(extracted_dir_path, PACKAGE_MAIN_MODULE)
         if not extracted_module_path:
-            message = f'Extracted files are missing core module.\nExtraction Path: "{extract_path}".'
+            message = f'Extracted files are missing core module.\nExtraction Path: "{cache_extract}".'
             self.progress_win.add_text_to_output_box(input_string=message, color=resource_library.Color.Hex.red_soft)
             return
 
@@ -264,37 +285,14 @@ class PackageUpdaterModel:
             self.progress_win.set_progress_bar_done()
             self.progress_win.change_last_line_color(resource_library.Color.Hex.green_soft)
             feedback = feedback_utils.FeedbackMessage(intro="GT-Tools",
-                                                      style_intro=f"color:{resource_library.Color.Hex.lime};"
+                                                      style_intro=f"color:{resource_library.Color.Hex.cyan_soft};"
                                                                   f"text-decoration:underline;",
                                                       conclusion="has been updated and is now active.")
             feedback.print_inview_message(stay_time=4000)
         else:
             self.progress_win.change_last_line_color(resource_library.Color.Hex.red_soft)
+        _cache.clear_cache()
 
-
-        # result = None
-        # try:
-        #     result = setup_utils.install_package(callbacks=[self.progress_win.add_text_to_output_box,
-        #                                                     self.progress_win.increase_progress_bar_value])
-        # except Exception as e:
-        #     self.progress_win.add_text_to_output_box(input_string=str(e), color=resource_library.Color.Hex.red_soft)
-        #
-        # # Installation Result
-        # if result:
-        #     self.update_version()
-        #     self.update_status()
-        #     self.progress_win.set_progress_bar_done()
-        #     self.progress_win.first_button.clicked.connect(self.close_view)  # Closes parent (Package Setup View)
-        #     self.progress_win.change_last_line_color(resource_library.Color.Hex.green_soft)
-        #     feedback = feedback_utils.FeedbackMessage(intro="GT-Tools",
-        #                                               style_intro=f"color:{self.package_name_color};"
-        #                                                           f"text-decoration:underline;",
-        #                                               conclusion="has been installed and is now active.")
-        #     feedback.print_inview_message(stay_time=4000)
-        # else:
-        #     self.progress_win.change_last_line_color(resource_library.Color.Hex.red_soft)
-        #
-        # Show window
         if QApplication.instance():
             try:
                 sys.exit(app.exec_())
@@ -308,7 +306,7 @@ if __name__ == "__main__":
     import maya.standalone
     # maya.standalone.initialize()
     model = PackageUpdaterModel()
-    # model.check_for_updates()
+    model.check_for_updates()
     model.update_package()
     # print(model.__dict__)
 
