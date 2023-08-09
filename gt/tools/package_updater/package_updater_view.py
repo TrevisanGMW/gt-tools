bfcddb9f753dda4a8d172544542f47c55353be7a
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-08 19:09:46 -0700

    Created version and initial UI layout

--- gt/tools/package_updater/package_updater_view.py
+++ gt/tools/package_updater/package_updater_view.py
@@ -1,17 +1,16 @@
 """
 Curve Library Window - The main GUI window class for the Curve Library tool.
 """
-from PySide2.QtWidgets import QListWidget, QPushButton, QDialog, QWidget, QSplitter, QLineEdit, QDesktopWidget, QLabel
-from PySide2.QtGui import QIcon, QPainter, QPixmap
+from PySide2.QtWidgets import QPushButton, QDialog, QLineEdit, QLabel
 import gt.ui.resource_library as resource_library
-from PySide2.QtCore import QRect, QSize
 from PySide2 import QtWidgets, QtCore
 import gt.ui.qt_utils as qt_utils
+from PySide2.QtGui import QIcon
 import sys
 
 
 class PackageUpdaterView(QDialog):
-    def __init__(self, parent=None, controller=None):
+    def __init__(self, parent=None, controller=None, version=None):
         """
         Initialize the PackageUpdater.
         This window represents the main GUI window of the tool.
@@ -20,6 +19,7 @@ class PackageUpdaterView(QDialog):
             parent (str): Parent for this window
             controller (PackageUpdaterController): PackageUpdaterController, not to be used, here so it's not deleted
                                                    by the garbage collector.  Defaults to None.
+            version (str, optional): If provided, it will be used to determine the window title. e.g. Title - (v1.2.3)
         """
         super().__init__(parent=parent)
         self.controller = controller  # Only here so it doesn't get deleted by the garbage collectors
@@ -30,7 +30,12 @@ class PackageUpdaterView(QDialog):
         self.web_response_label = None
         self.installed_version_label = None
         self.latest_release_label = None
-        self.changelog_box_title = None
+        self.changelog_label = None
+        # Content
+        self.status_content = None
+        self.web_response_content = None
+        self.installed_version_content = None
+        self.latest_release_content = None
         # Buttons
         self.update_button = None
         self.refresh_button = None
@@ -39,8 +44,10 @@ class PackageUpdaterView(QDialog):
         # Misc
         self.changelog_box = None
 
-        self.setWindowTitle("Package Updater")
-        self.setGeometry(100, 100, 400, 300)
+        window_title = "Package Updater"
+        if version:
+            window_title += f' - (v{str(version)})'
+        self.setWindowTitle(window_title)
 
         self.create_widgets()
         self.create_layout()
@@ -54,25 +61,51 @@ class PackageUpdaterView(QDialog):
         stylesheet += resource_library.Stylesheet.maya_basic_dialog
         stylesheet += resource_library.Stylesheet.dark_list_widget
         self.setStyleSheet(stylesheet)
-        qt_utils.resize_to_screen(self)
+        qt_utils.resize_to_screen(self, percentage=35, width_percentage=30)
         qt_utils.center_window(self)
         # self.setWindowFlag(QtCore.Qt.Tool, True)  # Stay On Top Modality - Fixes Mac order issue
 
     def create_widgets(self):
         """Create the widgets for the window."""
-        self.title_label = QtWidgets.QLabel("Package Updater")
-        self.title_label.setStyleSheet('background-color: rgb(93, 93, 93); \
-                                                border: 0px solid rgb(93, 93, 93); \
-                                                color: rgb(255, 255, 255);\
-                                                font: bold 12px; \
-                                                padding: 5px;')
+        self.title_label = QtWidgets.QLabel("GT-Tools - Package Updater")
+        self.title_label.setStyleSheet('background-color: rgb(93, 93, 93); border: 0px solid rgb(93, 93, 93); \
+                                        color: rgb(255, 255, 255); padding: 5px; ')
         self.status_label = QLabel("Status:")
+        self.status_content = QLabel("<status>")
+
         self.web_response_label = QLabel("Web Response:")
+        self.web_response_content = QLabel("<web-response>")
+
         self.installed_version_label = QLabel("Installed Version:")
+        self.installed_version_content = QLabel("<installed-version>")
+
         self.latest_release_label = QLabel("Latest Release:")
-        self.changelog_box_title = QLabel("Latest Release Changelog:")
+        self.latest_release_content = QLabel("<latest-version>")
+
+        self.changelog_label = QLabel("Latest Release Changelog:")
+        self.changelog_label.setStyleSheet("font-weight: bold; font-size: 8pt; margin-top: 10px")
 
         self.changelog_box = QLineEdit()
+        self.changelog_box.setMinimumHeight(150)
+
+        # Set Alignment Center
+        for label in [self.title_label,
+                      self.status_label,
+                      self.status_content,
+                      self.web_response_label,
+                      self.web_response_content,
+                      self.installed_version_label,
+                      self.installed_version_content,
+                      self.latest_release_label,
+                      self.latest_release_content,
+                      self.changelog_label,
+                      ]:
+            label.setAlignment(QtCore.Qt.AlignCenter)
+            label.setFixedHeight(label.sizeHint().height())
+            label.setFont(qt_utils.get_font(resource_library.Font.roboto))
+
+        self.changelog_box.setSizePolicy(self.changelog_box.sizePolicy().Expanding,
+                                         self.changelog_box.sizePolicy().Expanding)
 
         self.update_button = QPushButton("Update")
         self.refresh_button = QPushButton("Refresh")
@@ -81,28 +114,40 @@ class PackageUpdaterView(QDialog):
 
     def create_layout(self):
         """Create the layout for the window."""
-        button_layout = QtWidgets.QVBoxLayout()
-        button_layout.addWidget(self.title_label)
-        button_layout.addWidget(self.status_label)
-        button_layout.addWidget(self.web_response_label)
-        button_layout.addWidget(self.installed_version_label)
-        button_layout.addWidget(self.latest_release_label)
-        button_layout.addWidget(self.changelog_box_title)
+        top_layout = QtWidgets.QHBoxLayout()
+        top_left_widget = QtWidgets.QVBoxLayout()
+        top_right_widget = QtWidgets.QVBoxLayout()
+        top_layout.addLayout(top_left_widget)
+        top_layout.addLayout(top_right_widget)
 
-        button_layout.addWidget(self.changelog_box)
+        top_left_widget.addWidget(self.status_label)
+        top_left_widget.addWidget(self.web_response_label)
+        top_left_widget.addWidget(self.installed_version_label)
+        top_left_widget.addWidget(self.latest_release_label)
 
-        button_layout.addWidget(self.update_button)
-        button_layout.addWidget(self.refresh_button)
-        button_layout.addWidget(self.interval_button)
-        button_layout.addWidget(self.auto_check_button)
+        top_right_widget.addWidget(self.status_content)
+        top_right_widget.addWidget(self.web_response_content)
+        top_right_widget.addWidget(self.installed_version_content)
+        top_right_widget.addWidget(self.latest_release_content)
 
-        main_layout = QtWidgets.QHBoxLayout(self)
+        bottom_layout = QtWidgets.QVBoxLayout()
+        bottom_layout.addWidget(self.changelog_label)
+        bottom_layout.addWidget(self.changelog_box)
+
+        bottom_layout.addWidget(self.auto_check_button)
+        bottom_layout.addWidget(self.interval_button)
+        bottom_layout.addWidget(self.refresh_button)
+        bottom_layout.addWidget(self.update_button)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addWidget(self.title_label)
         main_layout.setContentsMargins(15, 15, 15, 11)  # Make Margins Uniform LTRB
-        main_layout.addLayout(button_layout)
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(bottom_layout)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)  # Application - To launch without Maya
-    window = PackageUpdaterView()  # View
+    window = PackageUpdaterView(version="1.2.3")  # View
     window.show()  # Open Windows
     sys.exit(app.exec_())
