0ee708dfc331fb2b6654e45ba09eebc895227215
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-10 14:18:23 -0700

    Added UI update functions

--- gt/tools/package_updater/package_updater_view.py
+++ gt/tools/package_updater/package_updater_view.py
@@ -1,11 +1,12 @@
 """
 Curve Library Window - The main GUI window class for the Curve Library tool.
 """
-from PySide2.QtWidgets import QPushButton, QDialog, QLineEdit, QLabel
+from PySide2.QtWidgets import QPushButton, QDialog, QLabel, QTextEdit, QVBoxLayout, QHBoxLayout
 import gt.ui.resource_library as resource_library
-from PySide2 import QtWidgets, QtCore
+from PySide2 import QtWidgets, QtCore, QtGui
 import gt.ui.qt_utils as qt_utils
 from PySide2.QtGui import QIcon
+from PySide2.QtCore import Qt
 import sys
 
 
@@ -37,10 +38,10 @@ class PackageUpdaterView(QDialog):
         self.installed_version_content = None
         self.latest_release_content = None
         # Buttons
-        self.update_button = None
-        self.refresh_button = None
-        self.interval_button = None
-        self.auto_check_button = None
+        self.update_btn = None
+        self.refresh_btn = None
+        self.auto_check_btn = None
+        self.interval_btn = None
         # Misc
         self.changelog_box = None
 
@@ -61,7 +62,7 @@ class PackageUpdaterView(QDialog):
         stylesheet += resource_library.Stylesheet.maya_basic_dialog
         stylesheet += resource_library.Stylesheet.dark_list_widget
         self.setStyleSheet(stylesheet)
-        self.update_button.setStyleSheet(resource_library.Stylesheet.bright_push_button)
+        self.update_btn.setStyleSheet(resource_library.Stylesheet.bright_push_button)
         qt_utils.resize_to_screen(self, percentage=35, width_percentage=30)
         qt_utils.center_window(self)
         # self.setWindowFlag(QtCore.Qt.Tool, True)  # Stay On Top Modality - Fixes Mac order issue
@@ -84,11 +85,13 @@ class PackageUpdaterView(QDialog):
         self.latest_release_content = QLabel("<latest-version>")
 
         self.changelog_label = QLabel("Latest Release Changelog:")
-        self.changelog_label.setStyleSheet(f"font-weight: bold; font-size: 8pt; margin-top: 15; "
+        self.changelog_label.setStyleSheet(f"font-weight: bold; font-size: 8; margin-top: 15; "
                                            f"color: {resource_library.Color.RGB.grey_lighter};")
 
-        self.changelog_box = QLineEdit()
+        self.changelog_box = QTextEdit()
+        self.changelog_box.setFont(qt_utils.load_custom_font(resource_library.Font.roboto, point_size=9))
         self.changelog_box.setMinimumHeight(150)
+        self.changelog_box.setReadOnly(True)
 
         # Set Alignment Center
         for label in [self.title_label,
@@ -103,29 +106,35 @@ class PackageUpdaterView(QDialog):
                       self.changelog_label,
                       ]:
             label.setAlignment(QtCore.Qt.AlignCenter)
-            label.setFixedHeight(label.sizeHint().height())
+            label.setFixedHeight(label.sizeHint().height()*1.5)
             label.setFont(qt_utils.get_font(resource_library.Font.roboto))
+
+        self.changelog_label.setFixedHeight(35)
+        self.status_content.setFixedWidth(label.sizeHint().width())
+        self.web_response_content.setFixedWidth(label.sizeHint().width())
+        self.installed_version_content.setFixedWidth(label.sizeHint().width())
+        self.latest_release_content.setFixedWidth(label.sizeHint().width())
 
         self.changelog_box.setSizePolicy(self.changelog_box.sizePolicy().Expanding,
                                          self.changelog_box.sizePolicy().Expanding)
 
-        self.interval_button = QPushButton("Auto Check: Activated")
-        self.interval_button.setStyleSheet("padding: 10;")
-        self.interval_button.setToolTip("Change Check for Updates State (Activated/Deactivated)")
-        self.auto_check_button = QPushButton("Interval: 15 Days")
-        self.auto_check_button.setStyleSheet("padding: 10;")
-        self.auto_check_button.setToolTip("Change Check for Updates Interval (5, 10, 15 days)")
-        self.refresh_button = QPushButton("Refresh")
-        self.refresh_button.setStyleSheet("padding: 10; margin-bottom: 2;")
-        self.refresh_button.setToolTip("Check Github Again for New Updates")
-        self.update_button = QPushButton("Update")
-        self.update_button.setToolTip("Download and Install Latest Update")
+        self.auto_check_btn = QPushButton("Auto Check: Activated")
+        self.auto_check_btn.setStyleSheet("padding: 10;")
+        self.auto_check_btn.setToolTip("Change Check for Updates State (Activated/Deactivated)")
+        self.interval_btn = QPushButton("Interval: 15 Days")
+        self.interval_btn.setStyleSheet("padding: 10;")
+        self.interval_btn.setToolTip("Change Check for Updates Interval (5, 10, 15 days)")
+        self.refresh_btn = QPushButton("Refresh")
+        self.refresh_btn.setStyleSheet("padding: 10; margin-bottom: 2;")
+        self.refresh_btn.setToolTip("Check Github Again for New Updates")
+        self.update_btn = QPushButton("Update")
+        self.update_btn.setToolTip("Download and Install Latest Update")
 
     def create_layout(self):
         """Create the layout for the window."""
         top_layout = QtWidgets.QHBoxLayout()
-        top_left_widget = QtWidgets.QVBoxLayout()
-        top_right_widget = QtWidgets.QVBoxLayout()
+        top_left_widget = QVBoxLayout()
+        top_right_widget = QVBoxLayout()
         top_layout.addLayout(top_left_widget)
         top_layout.addLayout(top_right_widget)
 
@@ -138,18 +147,19 @@ class PackageUpdaterView(QDialog):
         top_right_widget.addWidget(self.web_response_content)
         top_right_widget.addWidget(self.installed_version_content)
         top_right_widget.addWidget(self.latest_release_content)
+        top_right_widget.setAlignment(Qt.AlignCenter)
 
-        bottom_layout = QtWidgets.QVBoxLayout()
+        bottom_layout = QVBoxLayout()
         bottom_layout.addWidget(self.changelog_label)
         bottom_layout.addWidget(self.changelog_box)
 
-        settings_buttons_layout = QtWidgets.QHBoxLayout()
-        settings_buttons_layout.addWidget(self.interval_button)
-        settings_buttons_layout.addWidget(self.auto_check_button)
-        settings_buttons_layout.setContentsMargins(0, 5, 0, 2)  # @@@
+        settings_buttons_layout = QHBoxLayout()
+        settings_buttons_layout.addWidget(self.auto_check_btn)
+        settings_buttons_layout.addWidget(self.interval_btn)
+        settings_buttons_layout.setContentsMargins(0, 5, 0, 2)
         bottom_layout.addLayout(settings_buttons_layout)
-        bottom_layout.addWidget(self.refresh_button)
-        bottom_layout.addWidget(self.update_button)
+        bottom_layout.addWidget(self.refresh_btn)
+        bottom_layout.addWidget(self.update_btn)
 
         main_layout = QtWidgets.QVBoxLayout(self)
         main_layout.addWidget(self.title_label)
@@ -164,12 +174,131 @@ class PackageUpdaterView(QDialog):
                 state (bool): New state of the button: Disable/Enable the button True = Active, False = Inactive.
         """
         if isinstance(state, bool):
-            self.update_button.setEnabled(state)
+            self.update_btn.setEnabled(state)
+
+    def add_text_to_changelog(self, text, text_color_hex=None):
+        """
+        Add text to the changelog_box with optional color selection.
+
+        Args:
+            text (str): The text to be added to the changelog_box.
+            text_color_hex (str, optional): The color of the text in HEX format (e.g., '#FF0000'). Defaults to None.
+        """
+        cursor = self.changelog_box.textCursor()
+        if text_color_hex:
+            formatted_text = f'<font color="{text_color_hex}">{text}</font>'
+        else:
+            formatted_text = text
+
+        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.insertHtml(formatted_text)
+        cursor.insertText("\n")  # Add a new line
+
+        self.changelog_box.setTextCursor(cursor)
+
+    @staticmethod
+    def update_label(label_obj, text, text_color_hex=None, bg_color_hex=None):
+        """
+        Update the status content and its background color.
+        This method updates the content and background color of a QLabel used
+        to display status information.
+
+        Args:
+            label_obj (QLabel): QLabel object to update
+            text (str): The new text to be displayed.
+            text_color_hex (str, optional): A hexadecimal color code for the text color. Default is None
+            bg_color_hex (str, optional): A hexadecimal color code for the background color. Default is None
+
+        Example:
+            To update the status label with "OK" status and green background color:
+            self.update_status("New Update Available!", "#00FF00")
+        """
+        label_obj.setText(text)
+        stylesheet = ''
+        if text_color_hex:
+            stylesheet += f'color: {text_color_hex};'
+        if bg_color_hex:
+            stylesheet += f'background-color: {bg_color_hex};'
+        if stylesheet:
+            label_obj.setStyleSheet(stylesheet)
+
+    def update_status(self, status, text_color_hex=None, bg_color_hex=None):
+        """
+        Updates the status content:
+        Args:
+            status (str): The new text to be displayed as status.
+            text_color_hex (str, optional): A hexadecimal color code for the text color. Default is None
+            bg_color_hex (str, optional): A hexadecimal color code for the background color. Default is None
+        """
+        self.update_label(self.status_content, status, text_color_hex=text_color_hex, bg_color_hex=bg_color_hex)
+
+    def update_web_response(self, response, text_color_hex=None, bg_color_hex=None):
+        """
+        Updates the status content:
+        Args:
+            response (str): The new text to be displayed as web-response.
+            text_color_hex (str, optional): A hexadecimal color code for the text color. Default is None
+            bg_color_hex (str, optional): A hexadecimal color code for the background color. Default is None
+        """
+        self.update_label(self.web_response_content, response, text_color_hex=text_color_hex, bg_color_hex=bg_color_hex)
+
+    def update_installed_version(self, version, text_color_hex=None, bg_color_hex=None):
+        """
+        Updates the status content:
+        Args:
+            version (str): The new text to be displayed as the installed version.
+            text_color_hex (str, optional): A hexadecimal color code for the text color. Default is None
+            bg_color_hex (str, optional): A hexadecimal color code for the background color. Default is None
+        """
+        self.update_label(self.installed_version_content, version,
+                          text_color_hex=text_color_hex,
+                          bg_color_hex=bg_color_hex)
+
+    def update_latest_release(self, version, text_color_hex=None, bg_color_hex=None):
+        """
+        Updates the status content:
+        Args:
+            version (str): The new text to be displayed as the installed version.
+            text_color_hex (str, optional): A hexadecimal color code for the text color. Default is None
+            bg_color_hex (str, optional): A hexadecimal color code for the background color. Default is None
+        """
+        self.update_label(self.latest_release_content, version,
+                          text_color_hex=text_color_hex,
+                          bg_color_hex=bg_color_hex)
+
+    def update_auto_check_status_btn(self, is_active):
+        """
+        Updates the status content:
+        Args:
+            is_active (bool): New status. True is activated, false is deactivated
+        """
+        title = "Auto Check: "
+        if is_active:
+            self.auto_check_btn.setText(f'{title}Activated')
+        else:
+            self.auto_check_btn.setText(f'{title}Deactivated')
+
+    def update_interval_button(self, num_days):
+        """
+        Updates the status content:
+        Args:
+            num_days (str, int): Number of days to update the interval button with.
+        """
+        if num_days:
+            self.interval_btn.setText(f'Interval: {str(num_days)} days')
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)  # Application - To launch without Maya
     window = PackageUpdaterView(version="1.2.3")  # View
     window.show()  # Open Windows
-    # window.change_update_button_state(state=False)
+    window.change_update_button_state(state=False)
+    window.add_text_to_changelog("hello hello helo hello")
+    window.add_text_to_changelog("hello hello helo hello", text_color_hex="#0000FF")
+    window.update_status("New Update Available!", text_color_hex="black", bg_color_hex="#FF7F7F")
+    window.update_web_response("OK")
+    window.update_installed_version("v1.2.3")
+    window.update_latest_release("v4.5.6")
+    window.update_auto_check_status_btn(False)
+    window.update_interval_button(5)
     sys.exit(app.exec_())
