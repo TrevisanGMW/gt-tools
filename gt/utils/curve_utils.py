2e5f97a30d8d883796478c51eaf5424a1ba73589
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-12 18:29:46 -0700

    Created ComplexCurve

--- gt/utils/curve_utils.py
+++ gt/utils/curve_utils.py
@@ -910,6 +910,80 @@ class CurveShape:
         return self.build(replace_crv=target_curve)
 
 
+class ComplexCurve(Curve):
+    def __init__(self, name=None, build_function=None):
+        """
+        Initializes a Curve object
+        Args:
+            name (str, optional): ComplexCurve transform name (shapes names are determined by the Callable function)
+        """
+        super().__init__(name=name)  # Call the parent class constructor
+        self.build_function = None
+        self.set_build_function(build_function=build_function)
+        self.last_callable_output = None
+
+    def set_build_function(self, build_function):
+        """
+        Sets the build function for this complex curve
+        Args:
+            build_function (callable): A function used to build the curve
+        """
+        if callable(build_function):
+            self.build_function = build_function
+
+    def build(self, *args, **kwargs):
+        """
+        Use the provided callable function to generate/create a Maya curve.
+        Also renames the generated curve using the string stored in "self.name"
+        If a list is returned from the "self.build_function" the parent transform is assumed to be the first object.
+        e.g. ["parent_transform", "curve_shape", "something_else"] = Returns "parent_transform"
+        Args:
+            args (any): Arguments to give the complex curve callable object.
+            kwargs (any): Keyword arguments to give the complex curve callable object.
+        Returns:
+            str or Any: Name of the transform of the newly generated curve. (Result of the callable function)
+                        "None" if curve is invalid (does not have a callable function)
+                        Any if "return_callable_output" is active.
+        """
+        if not self.is_curve_valid():
+            logger.warning("ComplexCurve object is missing a callable function.")
+            return
+        try:
+            callable_result = self.build_function(*args, **kwargs)
+            self.last_callable_output = callable_result
+        except Exception as e:
+            logger.warning(f'Unable to build complex curve. Build function raised an error: {e}')
+            raise e
+            return
+        parent_transform = None
+        if isinstance(callable_result, list) and len(callable_result) > 0:
+            parent_transform = callable_result[0]
+        elif isinstance(callable_result, str):
+            parent_transform = callable_result
+        if self.name and parent_transform:
+            parent_transform = cmds.rename(parent_transform, self.name)
+        return parent_transform
+
+    def is_curve_valid(self):
+        """
+        Checks if the Curve object has enough data to create/generate a curve.
+        Returns:
+            bool: True if it's valid (can create a curve), False if invalid.
+                  In this case it's valid if it has a callable function.
+        """
+        if self.build_function is not None:
+            return True
+        return False
+
+    def get_last_callable_output(self):
+        """
+        Returns the last output received from the build call
+        Returns:
+            any: Anything received as the last output from the callable function. If it was never called, it is None.
+        """
+        return self.last_callable_output
+
+
 class Curves:
     def __init__(self):
         """
@@ -1143,6 +1217,16 @@ class Curves:
     target_squared_thick = Curve(data_from_file=get_curve_path("target_squared_thick"))
     triangle_pyramid_flat_four_arrows = Curve(data_from_file=get_curve_path("triangle_pyramid_flat_four_arrows"))
     triangle_pyramid_flat_two_arrows = Curve(data_from_file=get_curve_path("triangle_pyramid_flat_two_arrows"))
+
+
+class ComplexCurves:
+    def __init__(self):
+        """
+        A library of complex curve objects. These a created using a callable function.
+        Use "build()" to create them in Maya.
+        """
+    from gt.utils.data.controls import cluster_driven
+    scalable_arrow = ComplexCurve(build_function=cluster_driven.create_scalable_arrow)
 
 
 def add_thumbnail_metadata_attr_to_selection():
@@ -1433,4 +1517,9 @@ if __name__ == "__main__":
     # print_code_for_crv_files()
     # write_curve_files_from_selection(target_dir=DataDirConstants.DIR_CURVES, overwrite=True)  # Extract Curve
     # generate_curves_thumbnails(target_dir=None, force=True)  # Generate Thumbnails - (target_dir=None = Desktop)
+
+    test = ComplexCurves.scalable_arrow
+    test.build()
+    print(test.get_last_callable_output())
+
     pprint(out)
---
+++ gt/utils/data/controls/cluster_driven.py
@@ -0,0 +1,92 @@
+import maya.cmds as cmds
+import logging
+
+# Logging Setup
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def create_scalable_arrow(curve_name='scalable_arrow', initial_scale=1):
+    """
+    Creates a curve in the shape of an arrow and rigs it so when scaling it up the curve doesn't lose its shape
+
+    Args:
+        curve_name (string): Name of the generated curve
+        initial_scale (float): Initial Scale of the curve
+
+    Returns:
+        list: A list with the generated elements: [curve_name, curve_scale_handle, rig_grp]
+    """
+    curve_transform = cmds.curve(name=curve_name,
+                                 p=[[0.0, 0.0, -1.428], [0.409, 0.0, -1.0], [0.205, 0.0, -1.0], [0.205, 0.0, 1.0],
+                                    [0.409, 0.0, 1.0], [0.0, 0.0, 1.428], [-0.409, 0.0, 1.0], [-0.205, 0.0, 1.0],
+                                    [-0.205, 0.0, -1.0], [-0.409, 0.0, -1.0], [0.0, 0.0, -1.428]], d=1)
+    curve_shape = cmds.listRelatives(curve_transform, s=True, f=True)[0]
+    curve_shape = cmds.rename(curve_shape, '{0}Shape'.format(curve_transform))
+    logger.debug(str(curve_shape))
+    # Set Initial Scale
+    cmds.setAttr(curve_transform + '.sx', initial_scale)
+    cmds.setAttr(curve_transform + '.sy', initial_scale)
+    cmds.setAttr(curve_transform + '.sz', initial_scale)
+    cmds.makeIdentity(curve_transform, apply=True, scale=True, rotate=True)
+
+    # Create Scale Curve
+    curve_scale_crv = cmds.curve(name=curve_name + '_scaleCrv',
+                                 p=[[0.0, 0.0, -1.0], [0.0, 0.0, -0.333], [0.0, 0.0, 0.333], [0.0, 0.0, 1.0]], d=3)
+    curve_scale_shape = cmds.listRelatives(curve_scale_crv, s=True, f=True)[0]
+    curve_scale_shape = cmds.rename(curve_scale_shape, '{0}Shape'.format(curve_scale_crv))
+    # Set Initial Scale
+    cmds.setAttr(curve_scale_crv + '.sx', initial_scale)
+    cmds.setAttr(curve_scale_crv + '.sy', initial_scale)
+    cmds.setAttr(curve_scale_crv + '.sz', initial_scale)
+    cmds.makeIdentity(curve_scale_crv, apply=True, scale=True, rotate=True)
+
+    # Create Clusters
+    cmds.select([curve_transform + '.cv[0:2]', curve_transform + '.cv[8:10]'], r=True)
+    cluster_start = cmds.cluster(name=curve_name + '_start', bs=1)
+
+    cmds.select(curve_transform + '.cv[3:7]', r=True)
+    cluster_end = cmds.cluster(name=curve_name + '_end', bs=1)
+
+    # Create Mechanics
+    start_point_on_curve_node = cmds.createNode('pointOnCurveInfo', name=curve_name + '_start_pointOnCurve')
+    end_point_on_curve_node = cmds.createNode('pointOnCurveInfo', name=curve_name + '_end_pointOnCurve')
+    cmds.setAttr(start_point_on_curve_node + '.parameter', 0)
+    cmds.setAttr(end_point_on_curve_node + '.parameter', 1)
+
+    cmds.connectAttr(curve_scale_shape + '.worldSpace', start_point_on_curve_node + '.inputCurve')
+    cmds.connectAttr(curve_scale_shape + '.worldSpace', end_point_on_curve_node + '.inputCurve')
+
+    start_curve_scale_grp = cmds.group(name=curve_name + '_curveScale_start_grp', world=True, empty=True)
+    end_curve_scale_grp = cmds.group(name=curve_name + '_curveScale_end_grp', world=True, empty=True)
+
+    cmds.delete(cmds.pointConstraint(cluster_start, start_curve_scale_grp))
+    cmds.delete(cmds.pointConstraint(cluster_end, end_curve_scale_grp))
+
+    cmds.connectAttr(start_point_on_curve_node + '.result.position', start_curve_scale_grp + '.translate')
+    cmds.connectAttr(end_point_on_curve_node + '.result.position', end_curve_scale_grp + '.translate')
+
+    curve_rig_grp = cmds.group(name=curve_name + '_setup_grp', world=True, empty=True)
+
+    cmds.createNode('pointOnCurveInfo', name=curve_name + '_start_pointOnCurve')
+
+    # Setup Hierarchy
+    cmds.parent(cluster_start[1], start_curve_scale_grp)
+    cmds.parent(cluster_end[1], end_curve_scale_grp)
+    cmds.parent(curve_scale_crv, curve_rig_grp)
+    cmds.parent(start_curve_scale_grp, curve_rig_grp)
+    cmds.parent(end_curve_scale_grp, curve_rig_grp)
+
+    # Set Visibility
+    cmds.setAttr(cluster_start[1] + '.v', 0)
+    cmds.setAttr(cluster_end[1] + '.v', 0)
+    cmds.setAttr(curve_scale_crv + '.v', 0)
+
+    # Clean Selection
+    cmds.select(d=True)
+    return [curve_transform, curve_scale_crv, curve_rig_grp]
+
+
+if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
