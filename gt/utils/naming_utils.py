3d20b4a52c94e4e396a4dd89987c20188e3f389f
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-24 13:52:21 -0700

    Added naming convention description

--- gt/utils/naming_utils.py
+++ gt/utils/naming_utils.py
@@ -14,8 +14,15 @@ class NamingConstants:
     def __init__(self):
         """
         Naming Constants. Must be a string.
-        These are expected naming strings, such as prefixes, suffixes or anything that will help  describe an object.
-
+        These are expected naming strings, such as prefixes, suffixes or anything that will help describe an object.
+        Default naming convention:
+            <side>_<position>_<description><sequence>_<type>
+            <side> : Initial side of the element. e.g. "left", "right", "center" (usually X+ vs X-)
+            <position> : Position of the element. e.g. "mid", "upper", "lower"...
+            <description> : camelCase description of the object. e.g. "circleDirection"
+            <sequence> : multiple objects with the same name, may include a number or letter. e.g. "01" or "B"
+            <type> : object type (what it represents in the scene) e.g. "jnt" for joint or "grp" for group.
+            e.g. "lf_mid_eyebrow01_jnt", "cn_hip_jnt", "cn_jaw_jnt"
         """
     class Prefix:
         # Prefixes
