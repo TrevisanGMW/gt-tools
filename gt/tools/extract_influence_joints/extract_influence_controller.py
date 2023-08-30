cd1a162355bb19e87b1484b722bc51ae4bbfba7f
Guilherme Trevisan <trevisangmw@gmail.com>
2023-08-30 16:45:16 -0700

    Updated extract influence controller to new pattern

---
+++ gt/tools/extract_influence_joints/extract_influence_controller.py
@@ -0,0 +1,187 @@
+"""
+Extract Influence Controller
+"""
+from gt.utils.system_utils import execute_python_code
+from gt.utils.misc_utils import create_shelf_button
+from gt.utils.feedback_utils import FeedbackMessage
+from gt.utils.skin_utils import get_bound_joints
+import logging
+
+# Logging Setup
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class ExtractInfluenceController:
+    def __init__(self, view, model=None):
+        """
+        Initialize the ExtractInfluenceController object.
+
+        Args:
+            view: The view object to interact with the user interface.
+            model: The ExtractInfluenceModel object used for data manipulation.
+        """
+        self.model = model
+        self.view = view
+        self.view.controller = self
+
+        # # Connections
+        self.view.help_btn.clicked.connect(self.open_help)
+        self.view.extract_influence_python_btn.clicked.connect(self.extract_influence_python)
+        self.view.extract_influence_set_btn.clicked.connect(self.extract_influence_set)
+        self.view.run_code_btn.clicked.connect(self.run_python_code)
+        self.view.save_to_shelf_btn.clicked.connect(self.save_python_to_shelf)
+        self.view.bind_skin_btn.clicked.connect(self.open_bind_skin_options)
+        self.view.unbind_skin_btn.clicked.connect(self.run_unbind_skin)
+        # Initial State
+        self.view.non_existent_chk.setChecked(True)
+
+        self.view.show()
+
+    @staticmethod
+    def open_help():
+        """ Opens package docs """
+        from gt.utils.request_utils import open_package_docs_url_in_browser
+        open_package_docs_url_in_browser()
+
+    @staticmethod
+    def __get_selection():
+        """
+        Gets selection while warning the user in case nothing is elected
+        Returns:
+         list: Selection or empty list when nothing is selected.
+        """
+        import maya.cmds as cmds
+        selection = cmds.ls(selection=True) or []
+        if len(selection) == 0:
+            cmds.warning(f'Please select at least one object and try again.')
+            return []
+        return selection
+
+    def __extract_influence_with_validation(self, operation_target='python'):
+        """
+        Validation before extracting python or set out of the bound mesh
+        Args:
+            operation_target (optional, string): "python" will output python code into the python output box,
+                                                 "set" will create selection sets
+        Returns:
+            str or None: Returns the code to select influence joints or None there was an issue or creating a set.
+        """
+        import maya.cmds as cmds
+        selection = cmds.ls(selection=True) or []
+
+        if len(selection) == 0:
+            cmds.warning('Nothing selected. Please select a bound mesh and try again.')
+            return
+
+        valid_nodes = []
+        for sel in selection:
+            shapes = cmds.listRelatives(sel, shapes=True, children=False) or []
+            if shapes:
+                if cmds.objectType(shapes[0]) == 'mesh' or cmds.objectType(shapes[0]) == 'nurbsSurface':
+                    valid_nodes.append(sel)
+
+        if operation_target == 'python':
+            commands = []
+            for transform in valid_nodes:
+                message = '# Joint influences found in "' + transform + '":'
+                message += '\nbound_list = '
+                bound_joints = get_bound_joints(transform)
+
+                if not bound_joints:
+                    cmds.warning('Unable to find skinCluster for "' + transform + '".')
+                    return
+
+                if self.view.include_mesh_chk.isChecked():
+                    bound_joints.insert(0, transform)
+
+                message += str(bound_joints)
+
+                if self.view.non_existent_chk.isChecked():
+                    message += '\nbound_list = [jnt for jnt in bound_list if cmds.objExists(jnt)]'
+
+                message += '\ncmds.select(bound_list)'
+
+                commands.append(message)
+
+            _code = ''
+            for cmd in commands:
+                _code += cmd + '\n\n'
+            if _code.endswith('\n\n'):  # Removes unnecessary spaces at the end
+                _code = _code[:-2]
+            return _code
+
+        if operation_target == 'set':
+            for transform in valid_nodes:
+                bound_joints = get_bound_joints(transform)
+                if self.view.include_mesh_chk.isChecked():
+                    bound_joints.insert(0, transform)
+                new_set = cmds.sets(name=transform + "_influenceSet", empty=True)
+                for jnt in bound_joints:
+                    cmds.sets(jnt, add=new_set)
+
+    def extract_influence_python(self):
+        """
+        Extracts the TRS channels as setAttr commands and populates the python output box with the extracted content.
+        """
+        selection = self.__get_selection()
+        if not selection:
+            return
+        _code = "import maya.cmds as cmds\n\n"
+        _code += self.__extract_influence_with_validation(operation_target='python')
+        self.view.clear_python_output()
+        self.view.set_python_output_text(text=_code)
+
+    def extract_influence_set(self):
+        """
+        Extracts the TRS channels as lists and populates the python output box with the extracted content.
+        """
+        selection = self.__get_selection()
+        if not selection:
+            return
+        self.__extract_influence_with_validation(operation_target='set')
+
+    def run_python_code(self):
+        """
+        Attempts to run the code found in the "Python Output" box.
+        """
+        _code = self.view.get_python_output_text()
+        execute_python_code(code=_code, user_maya_warning=True)
+
+    def save_python_to_shelf(self):
+        """
+        Saves the content of the python output box to a shelf button.
+        """
+        import maya.cmds as cmds
+        _code = self.view.get_python_output_text()
+        if _code:
+            create_shelf_button(_code,
+                                label='setAttr',
+                                tooltip='Extracted Attributes',
+                                image="editRenderPass.png",
+                                label_color=(0, .84, .81))
+
+            highlight_style = "color:#FF0000;text-decoration:underline;"
+            feedback = FeedbackMessage(prefix='Python code',
+                                       style_prefix=highlight_style,
+                                       conclusion='was added as a button to your current shelf.')
+            feedback.print_inview_message()
+        else:
+            cmds.warning('Unable to save to shelf. "Output Python Code" is empty.')
+
+    @staticmethod
+    def open_bind_skin_options():
+        """ Runs the mel command to open the bind skin options """
+        import maya.mel as mel
+        mel.eval('SmoothBindSkinOptions;')
+
+    @staticmethod
+    def run_unbind_skin():
+        """ Runs the mel command unbind skins """
+        import maya.mel as mel
+        mel.eval('DetachSkin;')
+
+
+if __name__ == "__main__":
+    print('Run it from "__init__.py".')
---
+++ gt/tools/extract_influence_joints/extract_influence_view.py
@@ -0,0 +1,207 @@
+"""
+Extract Influence View/Window
+"""
+from PySide2.QtWidgets import QPushButton, QLabel, QVBoxLayout, QFrame, QCheckBox
+from gt.ui.syntax_highlighter import PythonSyntaxHighlighter
+from gt.ui.line_text_widget import LineTextWidget
+import gt.ui.resource_library as resource_library
+from gt.ui.qt_utils import MayaWindowMeta
+from PySide2 import QtWidgets, QtCore
+import gt.ui.qt_utils as qt_utils
+from PySide2.QtGui import QIcon
+from PySide2.QtCore import Qt
+
+
+class ExtractInfluenceView(metaclass=MayaWindowMeta):
+    def __init__(self, parent=None, controller=None, version=None):
+        """
+        Initialize the ExtractInfluenceView.
+        This window represents the main GUI window of the tool.
+
+        Args:
+            parent (str): Parent for this window
+            controller (ExtractInfluenceViewController): ExtractInfluenceViewController, not to be used, here so
+                                                          it's not deleted by the garbage collector.  Defaults to None.
+            version (str, optional): If provided, it will be used to determine the window title. e.g. Title - (v1.2.3)
+        """
+        super().__init__(parent=parent)
+
+        self.controller = controller  # Only here so it doesn't get deleted by the garbage collectors
+
+        # Window Title
+        self.window_title = "GT Extract Influence Joints"
+        _window_title = self.window_title
+        if version:
+            _window_title += f' - (v{str(version)})'
+        self.setWindowTitle(_window_title)
+
+        # Labels
+        self.title_label = None
+        self.output_python_label = None
+        # Buttons
+        self.help_btn = None
+        self.extract_influence_python_btn = None
+        self.extract_influence_set_btn = None
+        self.run_code_btn = None
+        self.save_to_shelf_btn = None
+        self.bind_skin_btn = None
+        self.unbind_skin_btn = None
+        self.unbind_skin_btn = None
+        self.non_existent_chk = None
+        self.include_mesh_chk = None
+        # Misc
+        self.output_python_box = None
+
+        self.create_widgets()
+        self.create_layout()
+
+        self.setWindowFlags(self.windowFlags() |
+                            QtCore.Qt.WindowMaximizeButtonHint |
+                            QtCore.Qt.WindowMinimizeButtonHint)
+        self.setWindowIcon(QIcon(resource_library.Icon.tool_attributes_to_python))
+
+        stylesheet = resource_library.Stylesheet.scroll_bar_dark
+        stylesheet += resource_library.Stylesheet.maya_basic_dialog
+        stylesheet += resource_library.Stylesheet.list_widget_dark
+        stylesheet += resource_library.Stylesheet.checkbox_dark
+        self.setStyleSheet(stylesheet)
+        self.extract_influence_python_btn.setStyleSheet(resource_library.Stylesheet.push_button_bright)
+        self.extract_influence_set_btn.setStyleSheet(resource_library.Stylesheet.push_button_bright)
+        qt_utils.resize_to_screen(self, percentage=40, width_percentage=55)
+        qt_utils.center_window(self)
+
+    def create_widgets(self):
+        """Create the widgets for the window."""
+        self.title_label = QtWidgets.QLabel(self.window_title)
+        self.title_label.setStyleSheet('background-color: rgb(93, 93, 93); border: 0px solid rgb(93, 93, 93); \
+                                        color: rgb(255, 255, 255); padding: 10px; margin-bottom: 0; text-align: left;')
+        self.title_label.setFont(qt_utils.get_font(resource_library.Font.roboto))
+        self.help_btn = QPushButton('Help')
+        self.help_btn.setToolTip("Open Help Dialog.")
+        self.help_btn.setStyleSheet('color: rgb(255, 255, 255); padding: 10px; '
+                                    'padding-right: 15px; padding-left: 15px; margin: 0;')
+        self.help_btn.setFont(qt_utils.get_font(resource_library.Font.roboto))
+
+        self.output_python_label = QLabel("Output Python Code:")
+        self.output_python_label.setStyleSheet(f"font-weight: bold; font-size: 8; margin-top: 0; "
+                                               f"color: {resource_library.Color.RGB.gray_lighter};")
+
+        self.output_python_box = LineTextWidget(self)
+
+        self.output_python_box.setMinimumHeight(150)
+        PythonSyntaxHighlighter(self.output_python_box.get_text_edit().document())
+
+        self.output_python_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.output_python_label.setFont(qt_utils.get_font(resource_library.Font.roboto))
+
+        self.output_python_box.setSizePolicy(self.output_python_box.sizePolicy().Expanding,
+                                             self.output_python_box.sizePolicy().Expanding)
+
+        self.extract_influence_python_btn = QPushButton('Extract Influence Joints to Python')
+        self.extract_influence_python_btn.setToolTip("Extracts bound joints (influence) as python code.")
+        self.extract_influence_set_btn = QPushButton("Extract Influence Joints to Selection Set")
+        self.extract_influence_set_btn.setToolTip('Extracts bound joints (influence) to selection set.')
+        self.run_code_btn = QPushButton("Run Code")
+        self.run_code_btn.setStyleSheet("padding: 10;")
+        self.save_to_shelf_btn = QPushButton("Save to Shelf")
+        self.save_to_shelf_btn.setStyleSheet("padding: 10;")
+        self.bind_skin_btn = QPushButton("Bind Skin")
+        self.bind_skin_btn.setStyleSheet("padding: 10;")
+        self.unbind_skin_btn = QPushButton("Unbind Skin")
+        self.unbind_skin_btn.setStyleSheet("padding: 10;")
+        self.non_existent_chk = QCheckBox("Include Non-Existent Filter")
+        self.include_mesh_chk = QCheckBox("Include Bound Mesh")
+
+    def create_layout(self):
+        """Create the layout for the window."""
+
+        top_buttons_layout = QtWidgets.QVBoxLayout()
+        two_horizontal_chk_layout = QtWidgets.QHBoxLayout()
+
+        non_existent_chk_layout = QtWidgets.QVBoxLayout()
+        non_existent_chk_layout.setAlignment(Qt.AlignCenter)
+        non_existent_chk_layout.addWidget(self.non_existent_chk)
+        include_mesh_chk_layout = QtWidgets.QVBoxLayout()
+        include_mesh_chk_layout.setAlignment(Qt.AlignCenter)
+        include_mesh_chk_layout.addWidget(self.include_mesh_chk)
+        include_mesh_chk_layout.setContentsMargins(0, 20, 0, 20)
+        two_horizontal_chk_layout.addLayout(non_existent_chk_layout)
+        two_horizontal_chk_layout.addLayout(include_mesh_chk_layout)
+
+        two_horizontal_btn_layout = QtWidgets.QHBoxLayout()
+        two_horizontal_btn_layout.addWidget(self.extract_influence_python_btn)
+        two_horizontal_btn_layout.addWidget(self.extract_influence_set_btn)
+        top_buttons_layout.addLayout(two_horizontal_chk_layout)
+        top_buttons_layout.addLayout(two_horizontal_btn_layout)
+
+        mid_layout = QVBoxLayout()
+        mid_layout.addWidget(self.output_python_label)
+        mid_layout.addWidget(self.output_python_box)
+        mid_layout.setContentsMargins(0, 5, 0, 5)  # L-T-R-B
+
+        bottom_buttons_layout = QVBoxLayout()
+        two_horizontal_btn_layout = QtWidgets.QHBoxLayout()
+        two_horizontal_btn_layout.addWidget(self.run_code_btn)
+        two_horizontal_btn_layout.addWidget(self.save_to_shelf_btn)
+        two_horizontal_btn_layout.addWidget(self.bind_skin_btn)
+        two_horizontal_btn_layout.addWidget(self.unbind_skin_btn)
+        bottom_buttons_layout.addLayout(two_horizontal_btn_layout)
+
+        separator = QFrame()
+        separator.setFrameShape(QFrame.HLine)
+        separator.setFrameShadow(QFrame.Sunken)
+
+        title_layout = QtWidgets.QHBoxLayout()
+        title_layout.setSpacing(0)
+        title_layout.addWidget(self.title_label, 5)
+        title_layout.addWidget(self.help_btn)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        top_layout = QtWidgets.QVBoxLayout()
+        bottom_layout = QtWidgets.QVBoxLayout()
+        top_layout.addLayout(title_layout)
+        top_layout.addLayout(top_buttons_layout)
+        top_layout.setContentsMargins(15, 15, 15, 15)  # L-T-R-B
+        main_layout.addLayout(top_layout)
+        main_layout.addWidget(separator)
+        bottom_layout.addLayout(mid_layout)
+        bottom_layout.addLayout(bottom_buttons_layout)
+        bottom_layout.setContentsMargins(15, 0, 15, 15)  # L-T-R-B
+        main_layout.addLayout(bottom_layout)
+
+    def clear_python_output(self):
+        """ Removes all text from the changelog box """
+        self.output_python_box.get_text_edit().clear()
+
+    def set_python_output_text(self, text):
+        """
+        Add text to the python output box.
+
+        Args:
+            text (str): The text to set.
+        """
+        self.output_python_box.get_text_edit().setText(text)
+
+    def get_python_output_text(self):
+        """
+        Gets the plain text found in the python output box.
+
+        Returns:
+            str: Text found inside the python output text edit box.
+        """
+        return self.output_python_box.get_text_edit().toPlainText()
+
+    def close_window(self):
+        """ Closes this window """
+        self.close()
+
+
+if __name__ == "__main__":
+    import inspect
+    import sys
+
+    with qt_utils.QtApplicationContext():
+        window = ExtractInfluenceView(version="1.2.3")  # View
+        window.set_python_output_text(text=inspect.getsource(sys.modules[__name__]))
+        window.show()
