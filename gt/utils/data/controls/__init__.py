abd856f629063344520eb5da40e45b2c687415c4
Guilherme Trevisan <trevisangmw@gmail.com>
2023-09-13 11:21:46 -0700

    Updated docstrings

--- gt/utils/data/controls/__init__.py
+++ gt/utils/data/controls/__init__.py
@@ -5,7 +5,7 @@ The difference is that a control is usually composed of multiple elements or ext
 For example, a control might have extra attributes that allow for shape change or transform limits.
 
 Note: All controls return a "ControlData" object as their return value.
-"ControlData" can be found in "gt.utils.data.control_data"
+"ControlData" can be found in "gt.utils.data.controls.control_data"
 
 Note:
 If the control contains a keyword argument called "name" it will be automatically inherited by the Control object.
