db7c7404595056473504424e27b7b7c018bd3823
Guilherme Trevisan <trevisangmw@gmail.com>
2023-09-13 11:21:30 -0700

    Added DIR_PARAMETRIC_MESHES

--- gt/utils/data_utils.py
+++ gt/utils/data_utils.py
@@ -26,8 +26,9 @@ class DataDirConstants:
     DIR_DATA = os.path.join(DIR_UTILS, "data")
     DIR_SCRIPTS = os.path.join(DIR_DATA, 'scripts')
     DIR_CURVES = os.path.join(DIR_DATA, 'curves')
-    DIR_MESHES = os.path.join(DIR_DATA, 'meshes')
     DIR_CONTROLS = os.path.join(DIR_DATA, 'controls')
+    DIR_MESHES = os.path.join(DIR_DATA, 'meshes')
+    DIR_PARAMETRIC_MESHES = os.path.join(DIR_DATA, 'py_meshes')
 
 
 def write_data(path, data):
