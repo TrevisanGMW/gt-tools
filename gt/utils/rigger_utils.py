ec5fe1cfad844a61e43c75710e5e6d12caf7dc37
Guilherme Trevisan <trevisangmw@gmail.com>
2023-09-28 12:01:23 -0700

    Updates to the root curve

--- gt/utils/rigger_utils.py
+++ gt/utils/rigger_utils.py
@@ -4,9 +4,9 @@ github.com/TrevisanGMW/gt-tools
 """
 from gt.utils.attr_utils import add_separator_attr, set_attr, add_attr, hide_lock_default_attributes, connect_attr
 from gt.utils.uuid_utils import add_uuid_attribute, is_uuid_valid, is_short_uuid_valid, generate_uuid
+from gt.utils.color_utils import set_color_override_viewport, ColorConstants
 from gt.utils.curve_utils import Curve, get_curve, add_shape_scale_cluster
 from gt.utils.naming_utils import NamingConstants, get_long_name
-from gt.utils.color_utils import set_color_override_viewport
 from gt.utils.uuid_utils import find_object_with_uuid
 from gt.utils.control_utils import add_snapping_shape
 from gt.utils.string_utils import remove_prefix
@@ -1085,18 +1085,29 @@ def parent_proxies(proxy_list):
                 parent(source_objects=offset, target_parent=parent_proxy)
 
 
-def create_root_curve(name="root"):
+def create_root_curve(name="main", separator_name="options", top_group_name="rigger"):
+    """
+
+    """
+
+    selection = cmds.ls(selection=True)
     root_crv = get_curve('_rig_root')
     root_crv.set_name(name=name)
     root_transform = root_crv.build()
-    root_grp = cmds.group(empty=True, world=True, name="rigger_proxy_grp")
+    root_grp = cmds.group(empty=True, world=True, name=f"{top_group_name}_{NamingConstants.Suffix.GRP}")
     hide_lock_default_attributes(obj=root_grp)
-    add_separator_attr(target_object=root_transform, attr_name="proxyOptions")
+    add_separator_attr(target_object=root_transform, attr_name=separator_name)
     connect_attr(source_attr=f'{root_transform}.sy', target_attr_list=[f'{root_transform}.sx', f'{root_transform}.sz'])
     hide_lock_default_attributes(obj=root_transform, scale=False)
     set_attr_state(obj_list=root_transform, attr_list=['sx', 'sz'], hidden=True)
-    set_color_override_viewport(obj=root_transform, rgb_color=(1, 0, 0))
+    set_color_override_viewport(obj=root_transform, rgb_color=ColorConstants.CENTER)
     cmds.parent(root_transform, root_grp)
+    cmds.select(clear=True)
+    if selection:
+        try:
+            cmds.select(selection=True)
+        except Exception as e:
+            logger.debug(f'Unable to restore initial selection. Issue: {str(e)}')
 
 
 if __name__ == "__main__":
