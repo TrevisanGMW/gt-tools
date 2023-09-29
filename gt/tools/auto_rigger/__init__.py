8847c9e440f72f413d47870cf95314d164040acd
Guilherme Trevisan <trevisangmw@gmail.com>
2023-09-29 14:53:23 -0700

    Updated version suffix

--- gt/tools/auto_rigger/__init__.py
+++ gt/tools/auto_rigger/__init__.py
@@ -4,7 +4,7 @@
 """
 # Tool Version
 __version_tuple__ = (0, 0, 1)
-__version_suffix__ = ''
+__version_suffix__ = 'alpha'
 __version__ = '.'.join(str(n) for n in __version_tuple__) + __version_suffix__
 
 
