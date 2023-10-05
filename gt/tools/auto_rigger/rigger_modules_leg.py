67a019d2a0f78a04d23f076078d320d96517f7e8
Guilherme Trevisan <trevisangmw@gmail.com>
2023-10-04 20:48:44 -0700

    Updated leg proxies with line parent

--- gt/tools/auto_rigger/rigger_modules_leg.py
+++ gt/tools/auto_rigger/rigger_modules_leg.py
@@ -2,7 +2,7 @@
 Auto Rigger Leg Modules
 github.com/TrevisanGMW/gt-tools
 """
-from gt.tools.auto_rigger.rigger_framework import Proxy, ModuleGeneric
+from gt.tools.auto_rigger.rigger_framework import Proxy, ModuleGeneric, RiggerConstants
 import maya.cmds as cmds
 import logging
 
@@ -24,22 +24,31 @@ class ModuleBipedLeg(ModuleGeneric):
         hip = Proxy(name="hip")
         hip.set_position(y=84.5)
         hip.set_locator_scale(scale=0.4)
+
         knee = Proxy(name="knee")
         knee.set_position(y=47.05)
         knee.set_locator_scale(scale=0.5)
+        knee.add_line_parent(line_parent=hip)
+
         ankle = Proxy(name="ankle")
         ankle.set_position(y=9.6)
         ankle.set_locator_scale(scale=0.4)
+        ankle.add_line_parent(line_parent=knee)
+
         ball = Proxy(name="ball")
         ball.set_position(z=13.1)
         ball.set_locator_scale(scale=0.4)
+        ball.add_line_parent(line_parent=ankle)
+
         toe = Proxy(name="toe")
         toe.set_position(z=23.4)
         toe.set_locator_scale(scale=0.4)
         toe.set_parent_uuid(uuid=ball.get_uuid())
         toe.set_parent_uuid_from_proxy(parent_proxy=ball)
+
         heel_pivot = Proxy(name="heelPivot")
         heel_pivot.set_locator_scale(scale=0.1)
+        heel_pivot.add_line_parent(line_parent=ankle)
         self.proxies.extend([hip, knee, ankle, ball, toe, heel_pivot])
 
     # --------------------------------------------------- Misc ---------------------------------------------------
