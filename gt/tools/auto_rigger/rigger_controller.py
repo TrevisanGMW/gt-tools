fc2b3d59212e80479d257380e4cc9d05359398e8
Guilherme Trevisan <trevisangmw@gmail.com>
2023-11-08 13:36:02 -0800

    Created initial UI update connections

--- gt/tools/auto_rigger/rigger_controller.py
+++ gt/tools/auto_rigger/rigger_controller.py
@@ -1,7 +1,12 @@
 """
 Auto Rigger Controller
 """
+from gt.tools.auto_rigger.rigger_view import create_module_attr_widget
+from PySide2.QtWidgets import QTreeWidgetItem
+from PySide2.QtGui import QIcon
 import logging
+
+from gt.tools.auto_rigger import rig_framework
 
 # Logging Setup
 logging.basicConfig()
@@ -15,21 +20,56 @@ class RiggerController:
         Initialize the RiggerController object.
 
         Args:
-            model: The RiggerModel object used for data manipulation.
-            view: The view object to interact with the user interface.
+            model (RiggerModel): The RiggerModel object used for data manipulation.
+            view (RiggerView): The view object to interact with the user interface.
         """
         self.model = model
         self.view = view
         self.view.controller = self
 
-        # Connections
-        self.view.show()
+        self.populate_module_tree()
 
-    def get_selected_module(self):
-        pass
+        # Connections
+        self.view.module_tree.itemClicked.connect(self.item_clicked)
+
+        # Show
+        self.view.show()
 
     def populate_module_tree(self):
         self.view.clear_module_tree()
+        project = self.model.get_project()
+        icon_project = QIcon(project.icon)
+        project_item = QTreeWidgetItem([project.get_name()])
+        project_item.setIcon(0, icon_project)
+        project_item.setData(1, 0, project)
+        self.view.add_item_to_module_tree(project_item)
+
+        modules = self.model.get_modules()
+        for module in modules:
+            icon = QIcon(module.icon)
+            module_type = module.get_module_class_name(remove_module_prefix=True)
+            tree_item = QTreeWidgetItem([module_type])
+            tree_item.setIcon(0, icon)
+            tree_item.setData(1, 0, module)
+            project_item.addChild(tree_item)
+
+        self.view.expand_all_module_tree_items()
+
+    def item_clicked(self, item, column):
+        print("Clicked item:", item.text(column))
+        module = item.data(1, 0)
+        if isinstance(module, rig_framework.RigProject):
+            # module_widget = create_module_attr_widget(module)
+            # self.view.set_module_widget(module_widget)
+            print("Project")
+        if isinstance(module, rig_framework.ModuleGeneric):
+            module_widget = create_module_attr_widget(module)
+            self.view.set_module_widget(module_widget)
+        else:
+            self.view.clear_module_widget()
+
+    def get_selected_module(self):
+        pass
 
     def save_project(self):
         pass
