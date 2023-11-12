0911e3029e8480c54c7882af8abd546aa76a68bc
Guilherme Trevisan <trevisangmw@gmail.com>
2023-11-12 13:19:24 -0800

    Created "QHeaderWithWidgets"

--- gt/ui/qt_utils.py
+++ gt/ui/qt_utils.py
@@ -1,9 +1,9 @@
-from PySide2.QtWidgets import QApplication, QWidget, QDesktopWidget, QDialog, QMainWindow, QTreeWidget
+from PySide2.QtWidgets import QApplication, QWidget, QDesktopWidget, QDialog, QMainWindow, QTreeWidget, QHeaderView
 from PySide2.QtGui import QFontDatabase, QColor, QFont, QPixmap, QIcon
 from gt.utils.session_utils import is_script_in_interactive_maya
 from gt.utils.system_utils import is_system_macos
 from PySide2 import QtGui, QtCore, QtWidgets
-from PySide2.QtCore import QPoint
+from PySide2.QtCore import QPoint, Qt
 import logging
 import sys
 import os
@@ -657,6 +657,56 @@ def expand_all_tree_items_recursively(tree_widget):
     """
     for i in range(tree_widget.topLevelItemCount()):
         expand_tree_item_recursively(tree_widget.topLevelItem(i))
+
+
+class QHeaderWithWidgets(QHeaderView):
+    """
+    Subclass of QHeaderView with the ability to set custom widgets for individual sections.
+
+    Attributes:
+        widget_index_dict (dict): A dictionary to store custom widgets with their corresponding section indices.
+    """
+
+    def __init__(self, parent=None):
+        """
+        Constructor for QHeaderWithWidgets.
+
+        Args:
+            parent (QWidget): The parent widget. Defaults to None.
+        """
+        super().__init__(Qt.Horizontal, parent)
+        self.widget_index_dict = {}
+
+    def add_widget(self, index, widget):
+        """
+        Set a custom widget for a specific section.
+
+        Args:
+            index (int): The index of the section.
+            widget (QWidget): The custom widget to be set.
+        """
+        if not isinstance(index, int) or index < 0:
+            return
+        self.widget_index_dict[index] = widget
+        widget.setParent(self)
+        widget.setVisible(False)
+
+    def paintSection(self, painter, rect, logical_index):
+        """
+        Paint the section, and if a custom widget is set for the section, paint and display the widget.
+
+        Args:
+            painter (QPainter): The painter object for rendering.
+            rect (QRect): The rectangle defining the section's area.
+            logical_index (int): The logical index of the section.
+
+        """
+        super().paintSection(painter, rect, logical_index)
+        if logical_index in self.widget_index_dict:
+            widget = self.widget_index_dict.get(logical_index)
+            if widget:
+                widget.setVisible(True)
+                widget.setGeometry(rect)
 
 
 if __name__ == "__main__":
