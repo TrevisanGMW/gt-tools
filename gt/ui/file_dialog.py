96f06011b3185449d1d6db835f4137474943f132
Guilherme Trevisan <trevisangmw@gmail.com>
2023-11-20 19:33:51 -0800

    Fixed duplicated application issue

--- gt/ui/file_dialog.py
+++ gt/ui/file_dialog.py
@@ -1,5 +1,5 @@
+from utils.session_utils import is_script_in_interactive_maya
 from PySide2.QtWidgets import QFileDialog
-from gt.ui import qt_utils
 
 
 def file_dialog(caption="Open File",
@@ -29,51 +29,48 @@ def file_dialog(caption="Open File",
     Returns:
         str: Path to the selected file/directory or empty string if cancelled.
     """
-    with qt_utils.QtApplicationContext() as context:
-        if context.is_in_interactive_maya():  # Within Maya ----------------------------------
-            import maya.cmds as cmds
-            params = {"fileFilter": file_filter,
-                      "dialogStyle": 2,  # Use a custom file, which is consistent across platforms.,
-                      "fileMode": 0,  # Open File
-                      "okCaption": ok_caption,
-                      "cancelCaption": cancel_caption,
-                      "caption": caption,
-                      }
-            # Determine File Mode
-            if write_mode:
-                params["fileMode"] = 1  # Write File
-            if dir_only:
-                params["fileMode"] = 2  # Directories Only
-            if starting_directory and isinstance(starting_directory, str):
-                params["startingDirectory"] = starting_directory
+    if is_script_in_interactive_maya():  # Within Maya ----------------------------------
+        import maya.cmds as cmds
+        params = {"fileFilter": file_filter,
+                  "dialogStyle": 2,  # Use a custom file, which is consistent across platforms.,
+                  "fileMode": 1,  # Open File
+                  "okCaption": ok_caption,
+                  "cancelCaption": cancel_caption,
+                  "caption": caption,
+                  }
+        # Determine File Mode
+        if write_mode:
+            params["fileMode"] = 0  # Write File
+        if dir_only:
+            params["fileMode"] = 2  # Directories Only
+        if starting_directory and isinstance(starting_directory, str):
+            params["startingDirectory"] = starting_directory
 
-            file_name = cmds.fileDialog2(**params) or []
-            if len(file_name) > 0:
-                return file_name[0]
+        file_name = cmds.fileDialog2(**params) or []
+        if len(file_name) > 0:
+            return file_name[0]
+        return ""
+
+    else:  # Outside Maya ----------------------------------------------------------------
+        options = QFileDialog.Options()
+
+        params = {"filter": file_filter,
+                  "caption": caption,
+                  }
+
+        if starting_directory and isinstance(starting_directory, str):
+            params["startingDirectory"] = starting_directory
+        _file_dialog = QFileDialog(parent=parent)
+        if write_mode and not dir_only:  # Write File
+            file_path, _ = _file_dialog.getSaveFileName(options=options, **params)
+        elif dir_only:  # Dir Only
+            file_path, _ = _file_dialog.getExistingDirectory(options=options, **params)
+        else:
+            file_path, _ = _file_dialog.getOpenFileName(options=options, **params)
+        if file_path:
+            return file_path
+        else:
             return ""
-
-        else:  # Outside Maya ----------------------------------------------------------------
-            if not parent:
-                parent = context.get_parent()
-            options = QFileDialog.Options()
-
-            params = {"filter": file_filter,
-                      "caption": caption,
-                      }
-
-            if starting_directory and isinstance(starting_directory, str):
-                params["startingDirectory"] = starting_directory
-
-            _file_dialog = QFileDialog(parent=parent)
-            if write_mode and not dir_only:  # Write File
-                file_path, _ = _file_dialog.getOpenFileName(options=options, **params)
-            elif dir_only:  # Dir Only
-                file_path, _ = _file_dialog.getExistingDirectory(options=options, **params)
-
-            if file_path:
-                return file_path
-            else:
-                return ""
 
 
 if __name__ == "__main__":
