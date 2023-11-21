bb7e616de42b8254b0260641f7f2920f9f61de48
Guilherme Trevisan <trevisangmw@gmail.com>
2023-11-20 19:33:05 -0800

    Created save, load, new and clear functions for the project

--- gt/tools/auto_rigger/rigger_model.py
+++ gt/tools/auto_rigger/rigger_model.py
@@ -4,9 +4,7 @@ Auto Rigger Model
 from gt.tools.auto_rigger.template_biped import create_template_biped
 from gt.utils.data_utils import write_json, read_json_dict
 from gt.tools.auto_rigger.rig_framework import RigProject
-from gt.ui.file_dialog import file_dialog
 import logging
-
 
 # Logging Setup
 logging.basicConfig()
@@ -22,43 +20,57 @@ class RiggerModel:
         self.project = create_template_biped()  # TODO TEMP
         # self.project = RigProject()  # TODO TEMP
 
+    def clear_project(self):
+        """
+        Re-initializes the project to an empty one.
+        """
+        self.project = RigProject()
+
     def set_project(self, project):
+        """
+        Sets a new project.
+        Args:
+            project (RigProject): A new project to be stored in "self.project"
+        """
         if not project or not isinstance(project, RigProject):
             logger.debug(f'Unable to set project. Invalid input.')
             return
         self.project = project
 
     def get_project(self):
+        """
+        Gets the current project. (RigProject)
+        Returns:
+            RigProject: Current project stored in "self.project"
+        """
         return self.project
 
     def get_modules(self):
+        """
+        Gets the modules stored in the current project
+        Returns:
+            list: A list of modules. e.g. [ModuleGeneric, ModuleGeneric, ...]
+        """
         return self.project.get_modules()
 
-    def save_project_to_file(self):
-        file_path = file_dialog(caption="Save Rig Project",
-                                write_mode=False,
-                                starting_directory=None,
-                                file_filter="All Files (*);;",
-                                ok_caption="Save Project",
-                                cancel_caption="Cancel")
-        if file_path:
-            data = self.project.get_project_as_dict()
-            write_json(path=file_path, data=data)
+    def save_project_to_file(self, path):
+        """
+        Save the current project to the provided path (JSON format)
+        """
+        data = self.project.get_project_as_dict()
+        write_json(path=path, data=data)
 
-    def load_project_from_file(self):
-        file_path = file_dialog(caption="Open Rig Project",
-                                write_mode=False,
-                                starting_directory=None,
-                                file_filter="All Files (*);;",
-                                ok_caption="Open Project",
-                                cancel_caption="Cancel")
-        if file_path:
-            self.project = RigProject()
-            data = read_json_dict(file_path)
-            self.project.read_data_from_dict(data)
+    def load_project_from_file(self, path):
+        """
+        Loads a new project from the provided path (path should point to a project description (JSON)
+        Args:
+            path (str): Path to the project description (JSON format)
+        """
+        self.project = RigProject()
+        data = read_json_dict(path)
+        self.project.read_data_from_dict(data)
 
 
 if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     model = RiggerModel()
-    model.save_project_to_file()
